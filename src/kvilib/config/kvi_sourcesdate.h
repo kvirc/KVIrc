2d33c8d42fa0ad692b4af6c89cc71b75d1a25a6d
Alexey Uzhva <alexey@uzhva.ru>
2006-12-22 01:00:50 +0000

    *AUTOMATICALLY* updated KVI_SOURCES_DATE variable
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@66 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvilib/config/kvi_sourcesdate.h
+++ src/kvilib/config/kvi_sourcesdate.h
@@ -28,8 +28,8 @@
 
 #include "kvi_settings.h"
 
-#define KVI_SOURCES_DATE "20061221"
-#define KVI_SOURCES_DATE_NUMERIC 0x20061221
+#define KVI_SOURCES_DATE "20061222"
+#define KVI_SOURCES_DATE_NUMERIC 0x20061222
 
 // this is the date of the sources before that we should force a setup
 // this is usually changed only when a setup is really needed because
