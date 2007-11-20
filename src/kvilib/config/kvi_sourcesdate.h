a61302c537d972d8e22ef8d8b35a47a1055ac55e
Elvio Basello <hellvis69@gmail.com>
2007-11-20 18:22:01 +0000

    updated sources date
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@924 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvilib/config/kvi_sourcesdate.h
+++ src/kvilib/config/kvi_sourcesdate.h
@@ -28,8 +28,8 @@
 
 #include "kvi_settings.h"
 
-#define KVI_SOURCES_DATE "20071118"
-#define KVI_SOURCES_DATE_NUMERIC 0x20071118
+#define KVI_SOURCES_DATE "20071120"
+#define KVI_SOURCES_DATE_NUMERIC 0x20071120
 
 // this is the date of the sources before that we should force a setup
 // this is usually changed only when a setup is really needed because
