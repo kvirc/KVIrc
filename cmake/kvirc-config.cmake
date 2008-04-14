3180f4a83fc4552e2b893b3b5e9c6c487d3db520
Fabio Bas <ctrlaltca@gmail.com>
2008-04-14 21:26:06 +0000

    initial work on kvirc4-config
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@1485 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- cmake/kvirc-config.cmake
+++ cmake/kvirc-config.cmake
@@ -1,14 +1,16 @@
 #!/bin/sh
 #
-#   A script for retrieving the latest HUMOR build configuration
+#   A script for retrieving the latest kvirc build configuration
 #   Mainly used for building plugins out of the source tree
 #   The idea is "stolen" from the gtk-config and xmms-config scripts :)
+#
 #
 #   09-04-2000 Szymon Stefanek (Initial release)
 #   31-01-2002 Szymon Stefanek (Revised for KVIrc 3.0.0)
 #   27-07-2002 Szymon Stefanek (Revised again for KVIrc 3.0.0)
 #   01-06-2007 Szymon Stefanek (Revised for SST)
 #   ??-10-2007 Szymon Stefanek (Revised for Humor)
+#   14-04-2007 CtrlAltCa (adapted to cmake branch)
 #
 #   This program is FREE software. You can redistribute it and/or
 #   modify it under the terms of the GNU General Public License
@@ -29,20 +31,19 @@ set -e
 
 print_syntax()
 {
-	echo "humor-config (HuMoR @HUMOR_CONFIG_VERSION@)"
-	echo "	A script for retrieving the latest HuMoR build configuration"
+	echo "@PACKAGE@@VERSION_MAJOR@-config (KVIrc @VERSION_RELEASE@)"
+	echo "	A script for retrieving the latest KVIrc build configuration"
 	echo ""
-	echo "Syntax : humor-config [OPTIONS]"
+	echo "Syntax : @PACKAGE@@VERSION_MAJOR@-config [OPTIONS]"
 	echo "  options:"
-	echo "    --version         : Version of the HuMoR toolkit."
-	echo "    --prefix          : The prefix where the HuMoR toolkit was installed."
-	echo "    --includedirs     : The include directories you should use to compile stuff with HuMoR."
+	echo "    --version         : Version of the KVIrc toolkit."
+	echo "    --prefix          : The prefix where KVIrc was installed."
+	echo "    --includedirs     : The include directories you should use to compile stuff with KVIrc."
 	echo "    --includeflags    : The include directories above with -I prepended."
-	echo "    --librarydirs     : The library directories you should use to compile stuff with HuMoR."
-	echo "    --libraries       : The libraries you should use to compile stuff with HuMoR."
+	echo "    --librarydirs     : The library directories you should use to compile stuff with KVIrc."
+	echo "    --libraries       : The libraries you should use to compile stuff with KVIrc."
 	echo "    --libraryflags    : The library directories above with -L prepended "
 	echo "                        and the libraries above with -l prepended."
-	echo "    --definitions     : The compiler definitions you should use to compile stuff with HuMoR."
 	exit 0
 }
 
@@ -50,7 +51,7 @@ if test $# -eq 0; then
 	print_syntax 1 1>&2
 fi
 
-HUMOR_STUFF_TO_ECHO=""
+KVIRC_STUFF_TO_ECHO=""
 
 while test $# -gt 0; do
 	case "$1" in
@@ -60,35 +61,32 @@ while test $# -gt 0; do
 
 	case $1 in
 		--version)
-		HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO @HUMOR_CONFIG_VERSION@"
+		KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO @VERSION_RELEASE@"
 		;;
 		--prefix)
-		HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO @HUMOR_CONFIG_PREFIX@"
+		KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO @INSTALL_PREFIX@"
 		;;
 		--includedirs)
-		HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO @HUMOR_CONFIG_INCLUDEDIRS@"
+		KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO @KVIRC_CONFIG_INCLUDEDIRS@"
 		;;
 		--includeflags)
-		for d in @HUMOR_CONFIG_INCLUDEDIRS@; do
-			HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO -I$d"
+		for d in @KVIRC_CONFIG_INCLUDEDIRS@; do
+			KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO -I$d"
 		done
 		;;
 		--librarydirs)
-		HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO @HUMOR_CONFIG_LIBRARYDIRS@"
+		KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO @KVIRC_CONFIG_LIBRARYDIRS@"
 		;;
 		--libraries)
-		HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO @HUMOR_CONFIG_LIBRARIES@"
+		KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO @LIBS@"
 		;;
 		--libraryflags)
-		for d in @HUMOR_CONFIG_LIBRARYDIRS@; do
-			HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO -L$d"
+		for d in @KVIRC_CONFIG_LIBRARYDIRS@; do
+			KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO -L$d"
 		done
-		for d in @HUMOR_CONFIG_LIBRARIES@; do
-			HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO -l$d"
+		for d in "@LIBS@"; do
+			KVIRC_STUFF_TO_ECHO="$KVIRC_STUFF_TO_ECHO -l$d"
 		done
-		;;
-		--definitions)
-		HUMOR_STUFF_TO_ECHO="$HUMOR_STUFF_TO_ECHO @HUMOR_CONFIG_DEFINITIONS@"
 		;;
 		*)
 		print_syntax 1 1>&2
@@ -97,6 +95,6 @@ while test $# -gt 0; do
 	shift
 done
 
-if test -n "$HUMOR_STUFF_TO_ECHO"; then
-	echo -n $HUMOR_STUFF_TO_ECHO
+if test -n "$KVIRC_STUFF_TO_ECHO"; then
+	echo -n $KVIRC_STUFF_TO_ECHO
 fi
