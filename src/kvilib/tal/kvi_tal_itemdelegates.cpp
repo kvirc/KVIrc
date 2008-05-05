66aa4b5f5fb18973cbe75de5e711c185e88d0a57
Noldor <elfonol@gmail.com>
2008-05-05 17:42:42 +0000

    quick fix
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@1680 17fca916-40b9-46aa-a4ea-0a15b648b75c

---
+++ src/kvilib/tal/kvi_tal_itemdelegates.cpp
@@ -0,0 +1,79 @@
+//=============================================================================
+//
+//   File : kvi_tal_listbox.cpp
+//   Creation date : Mon Jan 22 2007 11:25:08 by Szymon Stefanek
+//
+//   This file is part of the KVirc irc client distribution
+//   Copyright (C) 2007 Szymon Stefanek (pragma at kvirc dot net)
+//
+//   This program is FREE software. You can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your opinion) any later version.
+//
+//   This program is distributed in the HOPE that it will be USEFUL,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, write to the Free Software Foundation,
+//   Inc. ,59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+//=============================================================================
+
+#define __KVILIB__
+#include "kvi_tal_itemdelegates.h"
+#include <QListWidget>
+#include <QPainter>
+#include <QAbstractTextDocumentLayout>
+
+
+
+
+void KviTalIconAndRichTextItemDelegate::paint( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const
+{
+	painter->save();
+	if (option.state & QStyle::State_Selected)
+	{
+		QPalette pal=option.palette;
+		QBrush brush=pal.highlight();
+		QColor col=brush.color();
+		col.setAlpha(127);
+		brush.setColor(col);
+		painter->fillRect(option.rect,brush);
+	}
+	QString text=index.data(Qt::DisplayRole).toString();
+	QPixmap pixmap;
+	QRect decorationRect;
+	QVariant value = index.data(Qt::DecorationRole);
+	QStyle::State state=option.state;
+	QRect rect=option.rect;
+	int afterIcon = LVI_BORDER + LVI_ICON_SIZE + LVI_SPACING;
+	QIcon ico=QIcon(value.value<QIcon>());
+	if (ico.isNull())
+	{
+		if (m_pDefaultPix) painter->drawPixmap(option.rect.x()+LVI_BORDER,option.rect.y()+LVI_BORDER,*m_pDefaultPix);
+	}
+	else
+		painter->drawPixmap(option.rect.x()+LVI_BORDER,option.rect.y()+LVI_BORDER,ico.pixmap(LVI_ICON_SIZE,LVI_ICON_SIZE));
+	QTextDocument doc;
+	doc.setHtml( text );
+	painter->translate(option.rect.x()+afterIcon,option.rect.y()+LVI_BORDER);
+	doc.setTextWidth(option.rect.width()-10);
+	QRect cliprect=QRect(QPoint(0,0),QSize(option.rect.width()-afterIcon,option.rect.height()-(LVI_BORDER*2)-4));
+	doc.drawContents(painter, cliprect);	
+	painter->restore();
+}
+
+QSize KviTalIconAndRichTextItemDelegate::sizeHint( const QStyleOptionViewItem & option, const QModelIndex & index ) const
+{
+	QString text=index.data(Qt::DisplayRole).toString();
+	QTextDocument doc;
+	doc.setHtml( text );
+	return QSize(((QListWidget*)parent())->viewport()->size().width(),doc.documentLayout()->documentSize().height() + (2 * LVI_BORDER));		
+
+}
+
+
+
---
+++ src/kvilib/tal/kvi_tal_itemdelegates.h
@@ -0,0 +1,54 @@
+#ifndef _KVI_TAL_DELEGATES_H_
+#define _KVI_TAL_DELEGATES_H_
+
+//=============================================================================
+//
+//   File : kvi_tal_listwidget.h
+//   Creation date : Mon May 05 2008 11:25:08 by Carbone Alessandro
+//
+//   This file is part of the KVirc irc client distribution
+//   Copyright (C) 2007 Szymon Stefanek (pragma at kvirc dot net)
+//
+//   This program is FREE software. You can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your opinion) any later version.
+//
+//   This program is distributed in the HOPE that it will be USEFUL,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, write to the Free Software Foundation,
+//   Inc. ,59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+//=============================================================================
+
+#include "kvi_settings.h"
+#include <QItemDelegate>
+#include <QListWidget>
+
+#define LVI_ICON_SIZE 32
+#define LVI_BORDER 4
+#define LVI_SPACING 8
+#define LVI_MINIMUM_TEXT_WIDTH 300
+#define LVI_MINIMUM_CELL_WIDTH (LVI_MINIMUM_TEXT_WIDTH + LVI_BORDER + LVI_ICON_SIZE + LVI_SPACING + LVI_BORDER)
+
+
+class KVILIB_API KviTalIconAndRichTextItemDelegate : public QItemDelegate
+{
+public:
+	KviTalIconAndRichTextItemDelegate(QListWidget * pWidget=0)
+		: QItemDelegate(pWidget), m_pDefaultPix(0) {};
+
+	~KviTalIconAndRichTextItemDelegate(){};
+	 QSize sizeHint(const QStyleOptionViewItem &option,const QModelIndex &index) const;
+	 void paint ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+	 void setDefaultIcon(QPixmap *pix){m_pDefaultPix=pix;};
+protected:
+	QPixmap *m_pDefaultPix;
+};
+
+
+#endif // _KVI_TAL_LISTWIDGET_H_
