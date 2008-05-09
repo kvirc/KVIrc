c23dc768722d49a32e170b66b9cc1d124d21eaf0
Noldor <elfonol@gmail.com>
2008-05-09 16:24:08 +0000

    fix compilation error
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@1721 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/ui/kvi_selectors.cpp
+++ src/kvirc/ui/kvi_selectors.cpp
@@ -368,7 +368,7 @@ QString KviFileSelector::text()
 	return m_pLineEdit->text();
 }
 
-#warning "This should be fixed: otherwise we have to pass a null pointer to call the class"
+//#warning "This should be fixed: otherwise we have to pass a null pointer to call the class"
 /* FIXME: to call the class having a blank line, we have to pass a null value
           to pOption
           QString * szOption = new QString();
