4b05c6c931493ea5b339cce88a034f8890ff2de8
Fabio Bas <ctrlaltca@gmail.com>
2008-07-11 19:10:16 +0000

    bumped stable version to 3.4.1
    git-svn-id: https://svn.kvirc.de/svn/tags/kvirc/3.4.0@2062 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- admin/gendoc.pl
+++ admin/gendoc.pl
@@ -45,7 +45,7 @@ $g_prefixes{'class'}="class";
 $g_prefixes{'module'}="module";
 $g_prefixes{'widget'}="widget";
 
-$g_version = "3.2.0.99";
+$g_version = "3.4.1";
 $g_filehandle="";
 $g_shortsIdx{"keyterms"}=0;
 $g_directory = "";
