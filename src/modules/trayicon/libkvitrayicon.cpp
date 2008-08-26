kvirc/KVIrc

commit 9847d30b8d2c12ec36be913a2e2e4dc0c80ac1ad

upported fix for #139 to trunk/4.0
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2274 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/modules/trayicon/libkvitrayicon.cpp
@@ -313,6 +313,7 @@ void KviTrayIcon::toggleParentFrame()
 			m_pFrm->showNormal();
 		else
 			m_pFrm->show();
+		m_pFrm->setWindowState(getPrevWindowState() & Qt::WindowMaximized);
 	}
 }
 

src/kvirc/ui/kvi_frame.cpp
@@ -1328,11 +1328,17 @@ void KviFrame::hideEvent ( QHideEvent * e)
 		if(e->spontaneous())
 		{
 
-		if(!dockExtension())
-		{
-			executeInternalCommand(KVI_INTERNALCOMMAND_TRAYICON_SHOW);
-		}
-			 QTimer::singleShot( 0, this, SLOT(hide()) );
+			if(!dockExtension())
+			{
+				executeInternalCommand(KVI_INTERNALCOMMAND_TRAYICON_SHOW);
+			}
+
+			if(dockExtension())
+			{
+				dockExtension()->setPrevWindowState(windowState());
+				QTimer::singleShot( 0, this, SLOT(hide()) );
+			}
+
 		}
 
 	}

src/kvirc/ui/kvi_frame.h
@@ -63,13 +63,13 @@ class KviTalPopupMenu;
 class KVIRC_API KviDockExtension
 {
 protected:
-	unsigned int m_uStoredWindowState;
+	Qt::WindowStates m_uStoredWindowState;
 public:
 	KviDockExtension() : m_uStoredWindowState(0){};
 	virtual ~KviDockExtension(){};
 public:
-	void setPrevWindowState(unsigned int state) { m_uStoredWindowState = state; };
-	unsigned int getPrevWindowState() { return m_uStoredWindowState; };
+	void setPrevWindowState(Qt::WindowStates state) { m_uStoredWindowState = state; };
+	Qt::WindowStates getPrevWindowState() { return m_uStoredWindowState; };
 	
 	virtual void refresh(){};
 	virtual void die(){ delete this; };

commit d75c6fd62b8a7e34e4434e83665b0771cc89c176

upported patch for regression introduced in r2274
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2302 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/modules/trayicon/libkvitrayicon.cpp
@@ -313,7 +313,7 @@ void KviTrayIcon::toggleParentFrame()
 			m_pFrm->showNormal();
 		else
 			m_pFrm->show();
-		m_pFrm->setWindowState(getPrevWindowState() & Qt::WindowMaximized);
+		m_pFrm->setWindowState(getPrevWindowState() & Qt::WindowMinimized);
 	}
 }
 