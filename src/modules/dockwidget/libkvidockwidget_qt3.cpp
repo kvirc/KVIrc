kvirc/KVIrc

commit 35bd2aafaf75cf4e62aadec5f037c16823e917f8

fix for #139, needs testing
git-svn-id: https://svn.kvirc.de/svn/branches/kvirc/3.4@2273 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/modules/dockwidget/libkvidockwidget_qt3.cpp
@@ -488,6 +488,7 @@ void KviDockWidget::toggleParentFrame()
 			m_pFrm->showNormal();
 		else
 			m_pFrm->show();
+		m_pFrm->setWindowState(getPrevWindowState() & Qt::WindowMaximized);
 	}
 }
 

src/kvirc/ui/kvi_frame.cpp
@@ -1451,11 +1451,16 @@ void KviFrame::hideEvent ( QHideEvent * e)
 		if(e->spontaneous())
 		{
 		
-		if(!dockExtension())
-		{
-			executeInternalCommand(KVI_INTERNALCOMMAND_DOCKWIDGET_SHOW);
-		}
-			 QTimer::singleShot( 0, this, SLOT(hide()) );	
+			if(!dockExtension())
+			{
+				executeInternalCommand(KVI_INTERNALCOMMAND_DOCKWIDGET_SHOW);
+			}
+	
+			if(dockExtension())
+			{
+				dockExtension()->setPrevWindowState(windowState());
+				QTimer::singleShot( 0, this, SLOT(hide()) );	
+			}
 		}
 		
 	}

commit effddb95d11f772b841beb995d167ab6ce8321cf

probably fix for a regression caused by r2273
git-svn-id: https://svn.kvirc.de/svn/branches/kvirc/3.4@2301 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/modules/dockwidget/libkvidockwidget_qt3.cpp
@@ -488,7 +488,7 @@ void KviDockWidget::toggleParentFrame()
 			m_pFrm->showNormal();
 		else
 			m_pFrm->show();
-		m_pFrm->setWindowState(getPrevWindowState() & Qt::WState_Maximized);
+		m_pFrm->setWindowState(getPrevWindowState() & Qt::WState_Minimized);
 	}
 }
 