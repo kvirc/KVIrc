kvirc/KVIrc

commit 6e293eea2da5c003faf8b9eb385db3b9a6b3b347

improved hungarian notation; stylish fixes
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2559 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/modules/editor/scripteditor.h
@@ -29,7 +29,7 @@
 #include "kvi_pointerlist.h"
 #include "kvi_selectors.h"
 #include "kvi_qcstring.h"
-#include <kvi_tal_textedit.h>
+#include "kvi_tal_textedit.h"
 #include "kvi_tal_popupmenu.h"
 
 #include <QCompleter>
@@ -39,13 +39,11 @@
 #include <QTextEdit>
 #include <QListWidget>
 #include <QCheckBox>
-// Q3PopupMenu
-
 #include <QSyntaxHighlighter>
 
 class QTimer;
 
-typedef KviPointerList<int> ColumnList;
+//typedef KviPointerList<int> ColumnList;
 
 class KviScriptEditorWidget : public QTextEdit
 {
@@ -55,47 +53,35 @@ public:
 	KviScriptEditorWidget(QWidget * pParent);
 	virtual ~KviScriptEditorWidget();
 public:
+	QString m_szFind;
+protected:
+	QCompleter  * m_pCompleter;
+	QStringList * m_pListModulesNames;
+	QStringList * m_pListCompletition;
+	QTimer      * m_pStartTimer;
+	QWidget     * m_pParent;
+	QString       m_szHelp;
+public:
 	void createCompleter(QStringList &list);
 	void loadCompleterFromFile();
-	QCompleter * completer() const;
+	QCompleter * completer() const { return m_pCompleter; };
 	QString textUnderCursor() const;
 	void updateOptions();
-	void find1();
-	QString m_szFind;
+	//void find1();
 	bool contextSensitiveHelp() const;
 public slots:
-	void insertCompletion(const QString& completion);
+	void insertCompletion(const QString & szCompletion);
 	void slotFind();
 	void slotHelp();
 	void slotReplace();
-
-
+protected slots:
+	void asyncCompleterCreation();
 signals:
 	void keyPressed();
 protected:
-	QCompleter * m_pCompleter;
-	QStringList * m_pListModulesNames;
-	QStringList * m_pListCompletition;
-	QTimer *m_pStartTimer;
-	void contextMenuEvent(QContextMenuEvent *event);
+	void contextMenuEvent(QContextMenuEvent * e);
 	virtual void keyPressEvent(QKeyEvent * e);
-	void mouseReleaseEvent(QMouseEvent *);
-	QWidget *m_pParent;
-	QString m_szHelp;
-	protected slots:
-		void asyncCompleterCreation();
-	
-};
-
-class KviScriptSyntaxHighlighter : public QSyntaxHighlighter
-{
-public:
-	KviScriptSyntaxHighlighter(KviScriptEditorWidget * pWidget);
-	virtual ~KviScriptSyntaxHighlighter();
-public:
-	QTextEdit *m_pTextEdit;
-	QTextEdit *textEdit(){return m_pTextEdit;}
-	void highlightBlock(const QString &text);
+	void mouseReleaseEvent(QMouseEvent * e);
 };
 
 class KviScriptEditorWidgetColorOptions : public QDialog
@@ -113,32 +99,45 @@ protected slots:
 	void okClicked();
 };
 
+class KviScriptEditorSyntaxHighlighter : public QSyntaxHighlighter
+{
+public:
+	KviScriptEditorSyntaxHighlighter(KviScriptEditorWidget * pWidget);
+	virtual ~KviScriptEditorSyntaxHighlighter();
+public:
+	QTextEdit * m_pTextEdit;
+public:
+	QTextEdit * textEdit(){ return m_pTextEdit; }
+	void highlightBlock(const QString & szText);
+};
+
 class KviScriptEditorImplementation : public KviScriptEditor
 {
 	Q_OBJECT
 public:
 	KviScriptEditorImplementation(QWidget * par);
 	virtual ~KviScriptEditorImplementation();
+public:
+	QLineEdit * m_pFindlineedit;
 protected:
 	KviScriptEditorWidget * m_pEditor;
 	QLabel                * m_pRowColLabel;
-	int                  m_lastCursorPos;
+	int                     m_lastCursorPos;
 public:
-	virtual void setText(const QString &txt);
-	virtual void getText(QString &txt);
-	virtual void setText(const KviQCString &txt);
-	virtual void getText(KviQCString &txt);
-	virtual void setFindText(const QString & text);
+	virtual void setText(const QString & szText);
+	virtual void setText(const KviQCString & szText);
+	virtual void text(QString & szText);
+	virtual void text(KviQCString & szText);
+	virtual void setFindText(const QString & szText);
 	virtual void setEnabled(bool bEnabled);
 	virtual void setFocus();
 	virtual bool isModified();
-	void setFindLineeditReadOnly(bool b);
-	void setCursorPosition(int);
-	int getCursor();
-	QLineEdit *m_pFindlineedit;
-	QLineEdit * getFindlineedit();
+	void setFindLineEditReadOnly(bool b);
+	void setCursorPosition(int iPos);
+	int cursor(){ return m_lastCursorPos; };
+	QLineEdit * findLineEdit(){ return m_pFindLineedit; };
 protected:
-	virtual void focusInEvent(QFocusEvent *e);
+	virtual void focusInEvent(QFocusEvent * e);
 	void loadOptions();
 	void saveOptions();
 protected slots:
@@ -147,43 +146,37 @@ protected slots:
 	void configureColors();
 	void updateRowColLabel();
 	void slotFind();
-	void slotReplaceAll(const QString &,const QString &);
+	void slotReplaceAll(const QString & szToReplace, const QString & szReplaceWith);
 	void slotInitFind();
 	void slotNextFind(const QString &);
 signals:
-	void find( const QString &);
-	void replaceAll( const QString &, const QString &);
+	void find(const QString &);
+	void replaceAll(const QString & szToReplace, const QString & szReplaceWith);
 	void initFind();
-	void nextFind(const QString &);
+	void nextFind(const QString & szText);
 };
 
 class KviScriptEditorReplaceDialog: public QDialog
 {
 	Q_OBJECT
 public:
-	KviScriptEditorReplaceDialog( QWidget* parent = 0, const char* name = 0);
+	KviScriptEditorReplaceDialog(QWidget * parent = 0, const char * name = 0);
 	~KviScriptEditorReplaceDialog();
-	QLineEdit *m_pFindlineedit;
-	QLineEdit *m_pReplacelineedit;
+public:
+	QLineEdit * m_pFindLineEdit;
+	QLineEdit * m_pReplaceLineEdit;
 protected:
-	QLabel *findlabel;
-	QLabel *replacelabel;
-	QPushButton *replacebutton;
-	QPushButton *replace;
-	QPushButton *findNext;
-
-//	QPushButton *cancelbutton;
-	QCheckBox *checkReplaceAll;
-	QWidget *m_pParent;
+	QPushButton * m_pReplaceButton;
+	QCheckBox   * m_pCheckReplaceAll;
+	QWidget     * m_pParent;
 protected slots:
 	void textChanged(const QString &);
 	void slotReplace();
 	void slotNextFind();
 signals:
-	void replaceAll( const QString &,const QString &);
+	void replaceAll(const QString &, const QString &);
 	void initFind();
 	void nextFind(const QString &);
 };
 
-
-#endif //!_SCRIPTEDITOR_H_
+#endif // _SCRIPTEDITOR_H_

commit 340ff2bdeaaa94f626544abc5d0e5cbf914d7935

commit by noldor; fix for a regression introduced in r2559
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2561 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/modules/editor/scripteditor.cpp
@@ -73,6 +73,11 @@ static QColor g_clrPunctuation(180,180,0);
 static QColor g_clrFind(0,0,0);
 static QFont g_fntNormal("Courier New",8);
 
+
+
+
+
+
 KviScriptEditorWidget::KviScriptEditorWidget(QWidget * pParent)
 : QTextEdit(pParent)
 {
@@ -114,11 +119,11 @@ void KviScriptEditorWidget::asyncCompleterCreation()
 		g_pApp->getGlobalKvircDirectory(szPath,KviApp::Plugins);
 		QDir d(szPath);
 #if defined(COMPILE_ON_WINDOWS)
-			d.setNameFilters(QStringList("kvi*.dll"));
+		d.setNameFilters(QStringList("kvi*.dll"));
 #elif defined(COMPILE_ON_MINGW)
-			d.setNameFilters(QStringList("libkvi*.dll"));
+		d.setNameFilters(QStringList("libkvi*.dll"));
 #else
-			d.setNameFilters(QStringList("libkvi*.so"));
+		d.setNameFilters(QStringList("libkvi*.so"));
 #endif
 		m_pListModulesNames = new QStringList(d.entryList(QDir::Files | QDir::Readable));
 		iModulesCount = m_pListModulesNames->count();
@@ -209,6 +214,8 @@ void KviScriptEditorWidget::insertCompletion(const QString & szCompletion)
 	tc.insertText(szTmp);
 	setTextCursor(tc);
 }
+
+
 
 void KviScriptEditorWidget::contextMenuEvent(QContextMenuEvent * e)
 {
@@ -302,16 +309,36 @@ void KviScriptEditorWidget::keyPressEvent(QKeyEvent * e)
 			break;
 		}
 	}
-
+// Adapted from QT4 QCompleter example
+	bool isShortcut = ((e->modifiers() & Qt::ControlModifier) && e->key() == Qt::Key_E); // CTRL+E
+	if (!m_pCompleter || !isShortcut) // dont process the shortcut when we have a completer
+         QTextEdit::keyPressEvent(e);
+	const bool ctrlOrShift = e->modifiers() & (Qt::ControlModifier | Qt::ShiftModifier);
+    if (!m_pCompleter || (ctrlOrShift && e->text().isEmpty()))
+         return;
+    static QString eow("~!@#$%^&*()_+{}|:\"<>?,/;'[]\\-="); // end of word
+    bool hasModifier = (e->modifiers() != Qt::NoModifier) && !ctrlOrShift;
+    QString completionPrefix = textUnderCursor();
+	if (!isShortcut && (hasModifier || e->text().isEmpty()|| completionPrefix.length() < 3 || eow.contains(e->text().right(1)))) 
+	{
+    	m_pCompleter->popup()->hide();
+		return;
+    }
+    if (completionPrefix != m_pCompleter->completionPrefix()) {
+        m_pCompleter->setCompletionPrefix(completionPrefix);
+         m_pCompleter->popup()->setCurrentIndex(m_pCompleter->completionModel()->index(0, 0));
+     }
+    QRect cr = cursorRect();
+    cr.setWidth(m_pCompleter->popup()->sizeHintForColumn(0)+ m_pCompleter->popup()->verticalScrollBar()->sizeHint().width());
+	m_pCompleter->complete(cr);
+	/*
 	bool bIsShortcut = ((e->modifiers() & Qt::ControlModifier) && e->key() == Qt::Key_E); // CTRL+E
 
 	// don't process the shortcut when we have a completer
-	if(!m_pCompleter || !bIsShortcut)
-		QTextEdit::keyPressEvent(e);
+	if(!m_pCompleter || !bIsShortcut)QTextEdit::keyPressEvent(e);
 
 	const bool bCtrlOrShift = e->modifiers() & (Qt::ControlModifier | Qt::ShiftModifier);
-	if(!m_pCompleter || (bCtrlOrShift && e->text().isEmpty()))
-		return;
+	if(!m_pCompleter || (bCtrlOrShift && e->text().isEmpty()))return;
 
 	// end of word
 	static QString szEow("~!@#$%^&*()_+{}|:\"<>?,/;'[]\\-=");
@@ -319,11 +346,7 @@ void KviScriptEditorWidget::keyPressEvent(QKeyEvent * e)
 
 	QString szCompletionPrefix = textUnderCursor();
 	debug ("completition prefix %s",szCompletionPrefix.toUtf8().data());
-	if(!bIsShortcut && (hasModifier || e->text().isEmpty()
-				|| szCompletionPrefix.length() < 3
-				|| szEow.contains(e->text().right(1))
-			)
-	)
+	if(!bIsShortcut && (hasModifier || e->text().isEmpty()|| szCompletionPrefix.length() < 3 || szEow.contains(e->text().right(1))))
 	{
 		m_pCompleter->popup()->hide();
 		return;
@@ -338,6 +361,7 @@ void KviScriptEditorWidget::keyPressEvent(QKeyEvent * e)
 	QRect cr = cursorRect();
 	cr.setWidth(m_pCompleter->popup()->sizeHintForColumn(0) + m_pCompleter->popup()->verticalScrollBar()->sizeHint().width());
 	m_pCompleter->complete(cr);
+	*/
 }
 
 QString KviScriptEditorWidget::textUnderCursor() const
@@ -1019,7 +1043,7 @@ void KviScriptEditorImplementation::setText(const KviQCString & szText)
 	updateRowColLabel();
 }
 
-void KviScriptEditorImplementation::text(KviQCString & szText)
+void KviScriptEditorImplementation::getText(KviQCString & szText)
 {
 	szText = m_pEditor->toPlainText().toUtf8();
 }
@@ -1027,13 +1051,14 @@ void KviScriptEditorImplementation::text(KviQCString & szText)
 void KviScriptEditorImplementation::setText(const QString & szText)
 {
 	m_pEditor->setText(szText);
-	//m_pEditor->setTextFormat(Qt::PlainText);
-	m_pEditor->textCursor().movePosition(QTextCursor::End);
+	QTextCursor cur=m_pEditor->textCursor();
+	cur.movePosition(QTextCursor::End);
+	m_pEditor->setTextCursor(cur);
 	m_pEditor->document()->setModified(false);
 	updateRowColLabel();
 }
 
-void KviScriptEditorImplementation::text(QString & szText)
+void KviScriptEditorImplementation::getText(QString & szText)
 {
 	szText = m_pEditor->toPlainText();
 }
@@ -1064,6 +1089,7 @@ void KviScriptEditorImplementation::updateRowColLabel()
 
 void KviScriptEditorImplementation::setCursorPosition(int iPos)
 {
+	debug ("Moving to %d",iPos);
 	QTextCursor cur = m_pEditor->textCursor();
 	cur.setPosition(iPos);
 	m_pEditor->setTextCursor(cur);

src/modules/editor/scripteditor.h
@@ -126,12 +126,13 @@ protected:
 public:
 	virtual void setText(const QString & szText);
 	virtual void setText(const KviQCString & szText);
-	virtual void text(QString & szText);
-	virtual void text(KviQCString & szText);
+	virtual void getText(QString & szText);
+	virtual void getText(KviQCString & szText);
 	virtual void setFindText(const QString & szText);
 	virtual void setEnabled(bool bEnabled);
 	virtual void setFocus();
 	virtual bool isModified();
+	int getCursor(){return m_pEditor->textCursor().position();};
 	void setFindLineEditReadOnly(bool b);
 	void setCursorPosition(int iPos);
 	int cursor(){ return m_lastCursorPos; };