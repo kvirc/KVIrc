4613fcaae3330d7a30a9cc54ded6c229cf0d870c
Noldor <elfonol@gmail.com>
2008-10-12 16:00:43 +0000

    fix macro
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2714 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/modules/objects/object_macros.h
+++ src/modules/objects/object_macros.h
@@ -46,8 +46,7 @@
 
 // TEST
 #define KVSO_REGISTER_HANDLER_NEW(__className,__szName) \
-	g_pKvs##__className##Class->registerFunctionHandler("##__szName##",(KviKvsObjectFunctionHandlerProc)(KVI_PTR2MEMBER(__className::__szName)));
-
+g_pKvs##__className##Class->registerFunctionHandler(#__szName,(KviKvsObjectFunctionHandlerProc)(KVI_PTR2MEMBER(__className::__szName)));
 
 #define KVSO_REGISTER_STANDARD_NOTHINGRETURN_HANDLER(__className,__szName) \
 	g_pKvs##__className##Class->registerStandardNothingReturnFunctionHandler(__szName);
