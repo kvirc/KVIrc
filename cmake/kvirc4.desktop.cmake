74ff50f9b3fa02104bb571aac00eace10a1f6c93
Elvio Basello <hellvis69@gmail.com>
2008-11-06 16:39:14 +0000

    applied patch, thanks to GlaDiaC
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2832 17fca916-40b9-46aa-a4ea-0a15b648b75c

---
+++ cmake/kvirc4.desktop.cmake
@@ -0,0 +1,7 @@
+Name=KVIrc 4
+GenericName=IRC Client
+Comment=Connect to Internet Relay Chat
+Exec=${KVIRC_BINARYNAME} -m %u
+Icon=kvirc
+MimeType=application/x-kva;application/x-kvt;
+Categories=Qt;Network;IRCClient;
