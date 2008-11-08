0b62f949d9712d740bef3d36921efb258de3b5b8
Elvio Basello <hellvis69@gmail.com>
2008-11-08 15:55:48 +0000

    applied patch to fix kvirc4.desktop, thanks to Arfrever
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2840 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- cmake/kvirc4.desktop.cmake
+++ cmake/kvirc4.desktop.cmake
@@ -1,7 +1,9 @@
+[Desktop Entry]
 Name=KVIrc 4
 GenericName=IRC Client
 Comment=Connect to Internet Relay Chat
 Exec=${KVIRC_BINARYNAME} -m %u
 Icon=kvirc
+Type=Application
 MimeType=application/x-kva;application/x-kvt;
 Categories=Qt;Network;IRCClient;
