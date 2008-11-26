c32185c109b368b2da9ffbe5ae5e858844e95230
Thomas Kalla <tom@git-kvirc.net>
2008-11-26 01:36:46 +0000

    Installation fixes for MacOS X
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@2924 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- cmake/kvi_sysconfig.h.cmake
+++ cmake/kvi_sysconfig.h.cmake
@@ -42,7 +42,10 @@
 #define KVIRC_VERSION_RELEASE "${CMAKE_KVIRC_VERSION_RELEASE}"
 #define KVIRC_VERSION_BRANCH "${CMAKE_KVIRC_VERSION_BRANCH}"
 #define KVIRC_RESOURCES_DIR "${CMAKE_KVIRC_RESOURCES_DIR}"
-#define KVIRC_MODULES_DIR "${CMAKE_KVIRC_MODULES_DIR}"
 
+// KVIrc detects the modules directory on macs by itself
+#ifndef Q_OS_MACX
+#define KVIRC_MODULES_DIR "${CMAKE_KVIRC_MODULES_DIR}"
+#endif
 
 #endif //!_kvi_sysconfig_h_
