b022a0e4ecbef4b7cbba0cbf2830abf7e584157b
Fabio Bas <ctrlaltca@gmail.com>
2009-04-23 10:06:06 +0000

    animated avatars: avoid double connect() on nick change
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@3181 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/ui/kvi_userlistview.cpp
+++ src/kvirc/ui/kvi_userlistview.cpp
@@ -1274,6 +1274,12 @@ bool KviUserListView::nickChange(const QString & szOldNick, const QString & szNe
 		kvi_time_t joint  = pEntry->m_joinTime;
 		bool bSelect      = pEntry->m_bSelected;
 		KviAvatar * pAv   = pEntry->m_pGlobalData->forgetAvatar();
+		if(pEntry->m_pConnectedAnimation)
+		{
+			pEntry->m_pConnectedAnimation->disconnect(SIGNAL(frameChanged()),
+				this,SLOT(animatedAvatarUpdated()));
+		}
+
 		KviIrcUserEntry::Gender gender = pEntry->m_pGlobalData->gender();
 		bool bBot = pEntry->m_pGlobalData->isBot();
 		part(szOldNick);
