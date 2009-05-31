5d29e880381f6bb3560665f52fb7d6a26d088d68
Fabio Bas <ctrlaltca@gmail.com>
2009-05-31 14:52:34 +0000

    added localization patch from Arfrever, thank u
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@3231 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- cmake/kvirc4.desktop.cmake
+++ cmake/kvirc4.desktop.cmake
@@ -1,7 +1,9 @@
 [Desktop Entry]
 Name=KVIrc 4
 GenericName=IRC Client
+GenericName[pl]=Klient IRC
 Comment=Connect to Internet Relay Chat
+Comment[pl]=Połącz się z Internet Relay Chat (IRC)
 Exec=${KVIRC_BINARYNAME} -m %u
 Icon=kvirc
 Type=Application
