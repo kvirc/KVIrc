d325c9cd9e88cd65ff54180b364d4e628a694874
Fabio Bas <ctrlaltca@gmail.com>
2009-07-12 14:50:48 +0000

    fix for #491, part 2
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@3334 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/modules/notifier/notifierwindowtab.cpp
+++ src/modules/notifier/notifierwindowtab.cpp
@@ -121,6 +121,9 @@ void KviNotifierWindowTab::mouseDoubleClickEvent(QMouseEvent *)
 
 	if(m_pWnd->mdiParent())
 	{
+		m_pWnd->frame()->raise();
+		m_pWnd->frame()->setFocus();
+		m_pWnd->frame()->setWindowState(m_pWnd->frame()->windowState() & ~Qt::WindowMinimized | Qt::WindowActive);
 		if(!m_pWnd->frame()->isVisible())
 			m_pWnd->frame()->show();
 	}
