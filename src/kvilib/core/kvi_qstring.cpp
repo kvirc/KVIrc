b751aac314da54404c2f33e392762577ffdaebcf
Fabio Bas <ctrlaltca@gmail.com>
2009-09-04 16:43:25 +0000

    fix for #554
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@3476 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvilib/core/kvi_qstring.cpp
+++ src/kvilib/core/kvi_qstring.cpp
@@ -150,20 +150,20 @@ namespace KviQString
 	QString makeSizeReadable(size_t bytes)
 	{
 		double size = bytes;
-		if(size<900)
+		if(size<1024)
 			return QString(__tr2qs("%1 bytes")).arg(size,0,'f',0);
 
 		size/=1024;
-		if(size<900)
+		if(size<1024)
 			return QString(__tr2qs("%1 KB")).arg(size,0,'f',3);
 
 		size/=1024;
-		if(size<900)
+		if(size<1024)
 			return QString(__tr2qs("%1 MB")).arg(size,0,'f',3);
 
 		//Pirated DVD?;)
 		size/=1024;
-		if(size<900)
+		if(size<1024)
 			return QString(__tr2qs("%1 GB")).arg(size,0,'f',3);
 
 		//Uhm.. We are downloading a whole internet:)))
