e60cc02ab4a5b665e208a76207f91e547b3607fa
Fabio Bas <ctrlaltca@gmail.com>
2009-11-20 15:33:37 +0000

    added dccvoice, dccvideo to $features
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@3622 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/kvs/kvi_kvs_corefunctions_af.cpp
+++ src/kvirc/kvs/kvi_kvs_corefunctions_af.cpp
@@ -995,6 +995,12 @@ namespace KviKvsCoreFunctions
 	#ifdef COMPILE_WEBKIT_SUPPORT
 			"Webkit",
 	#endif
+	#ifndef COMPILE_DISABLE_DCC_VIDEO
+			"DCCVideo",
+	#endif
+	#ifndef COMPILE_DISABLE_DCC_VOICE
+			"DCCVoice",
+	#endif
 			"Qt4",
 			"KVS",
 			0
