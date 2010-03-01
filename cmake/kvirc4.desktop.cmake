5fefcebfe0cf4261d7927080f526b264b0329a36
José Melo <mmodem00@netvisao.pt>
2010-03-01 23:51:25 +0000

    fix kvirc_pt.po file with error and add portuguese translation to kvirc.desktop file
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4077 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- cmake/kvirc4.desktop.cmake
+++ cmake/kvirc4.desktop.cmake
@@ -2,8 +2,10 @@
 Name=KVIrc 4
 GenericName=IRC Client
 GenericName[pl]=Klient IRC
+GenericName[pt]=Cliente IRC
 Comment=Connect to Internet Relay Chat
 Comment[pl]=Połącz się z Internet Relay Chat (IRC)
+Comment[pt]=Conectar ao IRC
 Exec=${KVIRC_BINARYNAME} -m %u
 Icon=kvirc
 Type=Application
