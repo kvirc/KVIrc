kvirc/KVIrc

commit f92c7d6e3b847d502e40bc0578d830d4919f09e4

Merge the use of multiple QString()::arg(QString) calls to avoid format strings Filter out any urlencoded path form dcc file names
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4317 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvilib/tal/kvi_tal_wizard.cpp
@@ -357,7 +357,7 @@ void KviTalWizard::setCurrentPage(KviTalWizardPageData * pData)
 		szSteps = "<nobr><font color=\"";
 		szSteps += clrMid.name();
 		szSteps += "\"><b>[";
-		szSteps += QString("Step %1 of %2").arg(pData->iVisibleIndex).arg(m_p->iEnabledPageCount);
+		szSteps += QString("Step %1 of %2").arg(pData->iVisibleIndex, m_p->iEnabledPageCount);
 		szSteps += "]</b></font></nobr>";
 	}
 

src/kvirc/kernel/kvi_ircurl.cpp
@@ -311,7 +311,7 @@ int KviIrcUrl::run(const QString& text,int contextSpec,KviConsole* pConsole)
 			szCommand.append(szJoinCommand);
 			szCommand.append("\" ");
 		}
-		szCommand.append(QString("%1 %2 ").arg(parts.szHost).arg(parts.iPort));
+		szCommand.append(QString("%1 %2 ").arg(parts.szHost, parts.iPort));
 
 		if(pConsole->connection()) {
 			KviServer* server = pConsole->connection()->target()->server();

src/kvirc/ui/kvi_console.cpp
@@ -307,7 +307,7 @@ void KviConsole::getUserTipText(const QString &nick,KviIrcUserEntry *e,QString &
 
 	if(e->avatar())
 	{
-		buffer += QString("<tr><td><center><img src=\"%1\" width=\"%2\"></center></td></tr>").arg(e->avatar()->localPath()).arg(e->avatar()->size().width());
+		buffer += QString("<tr><td><center><img src=\"%1\" width=\"%2\"></center></td></tr>").arg(e->avatar()->localPath(), e->avatar()->size().width());
 	}
 
 	if(e->hasRealName())

src/kvirc/ui/kvi_userlistview.cpp
@@ -1590,7 +1590,7 @@ void KviUserListView::maybeTip(KviUserListToolTip * pTip, const QPoint & pnt)
 				int iHours = iMins / 60;
 				iMins = iMins % 60;
 				szBuffer += "<tr><td bgcolor=\"#F0F0F0\"><nobr><font color=\"#000000\">";
-				szBuffer += __tr2qs("Quiet for <b>%1h %2m %3s</b>").arg(iHours).arg(iMins).arg(iSecs);
+				szBuffer += __tr2qs("Quiet for <b>%1h %2m %3s</b>").arg(iHours, iMins, iSecs);
 				szBuffer += "</font></nobr></td></tr>";
 			}
 			

src/modules/dcc/send.cpp
@@ -1256,14 +1256,14 @@ void KviDccFileTransfer::displayPaint(QPainter * p,int column, QRect rect)
 					if(iW2 > 0)p->fillRect(rect.left() + 5 + iL2,rect.top() + 5,iW2,10,bIsTerminated ? QColor(150,130,110) : QColor(220,170,100));
 					p->fillRect(rect.left() + 5,rect.top() + 5,iL2,10,bIsTerminated ? QColor(140,110,110) : QColor(200,100,100));
 
-					txt = QString(__tr2qs_ctx("%1 of %2 (%3%)","dcc")).arg(KviQString::makeSizeReadable(uAckedBytes)).arg(KviQString::makeSizeReadable(m_uTotalFileSize)).arg(dPerc2,0,'f',2);
+					txt = QString(__tr2qs_ctx("%1 of %2 (%3%)","dcc")).arg(KviQString::makeSizeReadable(uAckedBytes), KviQString::makeSizeReadable(m_uTotalFileSize)).arg(dPerc2,0,'f',2);
 				} else {
 					// we are receiving a file or not sending acks
 					double dPerc = (double)(((double)uTransferred) * 100.0) / (double)m_uTotalFileSize;
 					int iL = (int) ((((double)iW) * dPerc) / 100.0);
 					p->fillRect(rect.left() + 5,rect.top() + 5,iL,10,bIsTerminated ? QColor(140,110,110) : QColor(200,100,100));
 
-					txt = QString(__tr2qs_ctx("%1 of %2 (%3%)","dcc")).arg(KviQString::makeSizeReadable(uTransferred)).arg(KviQString::makeSizeReadable(m_uTotalFileSize)).arg(dPerc,0,'f',2);
+					txt = QString(__tr2qs_ctx("%1 of %2 (%3%)","dcc")).arg(KviQString::makeSizeReadable(uTransferred), KviQString::makeSizeReadable(m_uTotalFileSize)).arg(dPerc,0,'f',2);
 				}
 
 			} else {
@@ -1353,7 +1353,7 @@ QString KviDccFileTransfer::tipText()
 {
 	QString s;
 
-	s = QString("<table><tr><td bgcolor=\"#000000\"><font color=\"#FFFFFF\"><b>DCC %1 (ID %2)</b></font></td></tr>").arg(m_szDccType.ptr()).arg(id());
+	s = QString("<table><tr><td bgcolor=\"#000000\"><font color=\"#FFFFFF\"><b>DCC %1 (ID %2)</b></font></td></tr>").arg(m_szDccType.ptr(), id());
 
 	s += "<tr><td bgcolor=\"#404040\"><font color=\"#FFFFFF\">";
 	s += __tr2qs_ctx("Transfer Log","dcc");
@@ -1485,14 +1485,14 @@ void KviDccFileTransfer::connectionInProgress()
 //			if(TRIGGER_EVENT_5PARAM_RETVALUE(KviEvent_OnDCCSendConnected,this,m_pDescriptor->szPort.ptr(),m_pDescriptor->szFileName.ptr(),m_pDescriptor->szNick.ptr(),m_pDescriptor->szUser.ptr(),m_pDescriptor->szHost.ptr()));
 //		}
 //
-		m_szStatusString = __tr2qs_ctx("Contacting host %1 on port %2","dcc").arg(m_pDescriptor->szIp).arg(m_pDescriptor->szPort);
+		m_szStatusString = __tr2qs_ctx("Contacting host %1 on port %2","dcc").arg(m_pDescriptor->szIp, m_pDescriptor->szPort);
 		outputAndLog(m_szStatusString);
 		displayUpdate();
 		return;
 	}
 
 	// PASSIVE CONNECTION
-	m_szStatusString = __tr2qs_ctx("Listening on interface %1 port %2","dcc").arg(m_pMarshal->localIp()).arg(m_pMarshal->localPort());
+	m_szStatusString = __tr2qs_ctx("Listening on interface %1 port %2","dcc").arg(m_pMarshal->localIp(), m_pMarshal->localPort());
 	outputAndLog(m_szStatusString);
 
 	if(m_pDescriptor->bSendRequest)
@@ -1566,7 +1566,7 @@ void KviDccFileTransfer::connectionInProgress()
 					ip.toUtf8().data(),port.ptr(),
 					&(m_pDescriptor->szLocalFileSize),0x01);
 		}
-		outputAndLog(__tr2qs_ctx("Sent DCC %1 request to %2, waiting for remote client to connect...","dcc").arg(szReq.ptr()).arg(m_pDescriptor->szNick));
+		outputAndLog(__tr2qs_ctx("Sent DCC %1 request to %2, waiting for remote client to connect...","dcc").arg(szReq.ptr(), m_pDescriptor->szNick));
 	} else {
 		outputAndLog(__tr2qs_ctx("DCC %1 request not sent, awaiting manual connection","dcc").arg(m_szDccType.ptr()));
 	}
@@ -1693,8 +1693,8 @@ void KviDccFileTransfer::handleMarshalError(int err)
 
 void KviDccFileTransfer::connected()
 {
-	outputAndLog(__tr2qs_ctx("Connected to %1:%2","dcc").arg(m_pMarshal->remoteIp()).arg(m_pMarshal->remotePort()));
-	outputAndLog(__tr2qs_ctx("Local end is %1:%2","dcc").arg(m_pMarshal->localIp()).arg(m_pMarshal->localPort()));
+	outputAndLog(__tr2qs_ctx("Connected to %1:%2","dcc").arg(m_pMarshal->remoteIp(), m_pMarshal->remotePort()));
+	outputAndLog(__tr2qs_ctx("Local end is %1:%2","dcc").arg(m_pMarshal->localIp(), m_pMarshal->localPort()));
 
 	m_tTransferStartTime = kvi_unixTime();
 
@@ -1774,7 +1774,7 @@ bool KviDccFileTransfer::resumeAccepted(const char *filename,const char *port,co
 
 	if(ret != KviError_success)handleMarshalError(ret);
 	else {
-		m_szStatusString = __tr2qs_ctx("Contacting host %1 on port %2","dcc").arg(m_pDescriptor->szIp).arg(m_pDescriptor->szPort);
+		m_szStatusString = __tr2qs_ctx("Contacting host %1 on port %2","dcc").arg(m_pDescriptor->szIp, m_pDescriptor->szPort);
 		outputAndLog(m_szStatusString);
 		displayUpdate();
 	}

src/modules/options/optw_servers.cpp
@@ -772,7 +772,7 @@ KviServerDetailsWidget::KviServerDetailsWidget(QWidget * par,KviServer * s)
 	KviPointerList<KviProxy> * proxylist = g_pProxyDataBase->proxyList();
 	for(KviProxy * p = proxylist->first();p;p = proxylist->next())
 	{
-		m_pProxyEditor->insertItem(m_pProxyEditor->count(),QString("%1:%2").arg(p->hostname()).arg(p->port()));
+		m_pProxyEditor->insertItem(m_pProxyEditor->count(),QString("%1:%2").arg(p->hostname(), p->port()));
 	}
 	if(m_pProxyEditor->count() > (s->proxy()+2))
 		m_pProxyEditor->setCurrentIndex(s->proxy()+2);

src/kvilib/ext/avdevice/videodevicepool.cpp
@@ -733,7 +733,7 @@ void VideoDevicePool::loadDeviceConfig()
 		qint32 ctrl_value;
 		QString ctrl_key;
 		bool ok = false;
-		const QString key_control_start = QString::fromLocal8Bit( "Device %1 Input %2 Control " ).arg( m_videodevice[m_current_device].udi() ).arg( m_videodevice[m_current_device].currentInput() );
+		const QString key_control_start = QString::fromLocal8Bit( "Device %1 Input %2 Control " ).arg( m_videodevice[m_current_device].udi() ,  m_videodevice[m_current_device].currentInput() );
 		QStringList ctrl_keys = config.keyList().filter(key_control_start);
 		qDebug() << "Found" << ctrl_keys.size() << "saved values for video-controls";
 		foreach (ctrl_key, ctrl_keys)
@@ -800,7 +800,7 @@ void VideoDevicePool::saveCurrentDeviceConfig()
 		config.writeEntry( key_currentinput, m_videodevice[m_current_device].currentInput() );
 		// --- Save values of the controls ---:
 		qint32 ctrl_value;
-		const QString key_control_start = QString::fromLocal8Bit( "Device %1 Input %2 Control " ).arg( m_videodevice[m_current_device].udi() ).arg( m_videodevice[m_current_device].currentInput() );
+		const QString key_control_start = QString::fromLocal8Bit( "Device %1 Input %2 Control " ).arg( m_videodevice[m_current_device].udi() ,  m_videodevice[m_current_device].currentInput() );
 		// Save values of the numeric controls:
 		QList<NumericVideoControl> numCtrls = m_videodevice[m_current_device].getSupportedNumericControls();
 		NumericVideoControl numCtrl;

src/kvilib/ext/kvi_osinfo.cpp
@@ -353,14 +353,14 @@ static QString queryWinInfo(QueryInfo info)
 				szVersion+= QString("Service Pack 6a (Build %1)").arg( osvi.dwBuildNumber & 0xFFFF );
 			else // Windows NT 4.0 prior to SP6a
 			{
-				szVersion+= QString( "%1 (Build %2)").arg( osvi.szCSDVersion).arg( osvi.dwBuildNumber & 0xFFFF);
+				szVersion+= QString( "%1 (Build %2)").arg( osvi.szCSDVersion,  osvi.dwBuildNumber & 0xFFFF);
 			}
 
 			RegCloseKey( hKey );
 		}
 		else // not Windows NT 4.0
 		{
-			szVersion+= QString( "%1 (Build %2)").arg( osvi.szCSDVersion).arg( osvi.dwBuildNumber & 0xFFFF);
+			szVersion+= QString( "%1 (Build %2)").arg( osvi.szCSDVersion,  osvi.dwBuildNumber & 0xFFFF);
 		}
 
 		break;

commit 50ccd48823e6a21207e7b117c51cb53bdff09356

misc format string fixed (regression introduced in r4317)
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4322 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvilib/tal/kvi_tal_wizard.cpp
@@ -357,7 +357,7 @@ void KviTalWizard::setCurrentPage(KviTalWizardPageData * pData)
 		szSteps = "<nobr><font color=\"";
 		szSteps += clrMid.name();
 		szSteps += "\"><b>[";
-		szSteps += QString("Step %1 of %2").arg(pData->iVisibleIndex, m_p->iEnabledPageCount);
+		szSteps += QString("Step %1 of %2").arg(pData->iVisibleIndex).arg(m_p->iEnabledPageCount);
 		szSteps += "]</b></font></nobr>";
 	}
 

src/kvirc/kernel/kvi_ircurl.cpp
@@ -311,7 +311,7 @@ int KviIrcUrl::run(const QString& text,int contextSpec,KviConsole* pConsole)
 			szCommand.append(szJoinCommand);
 			szCommand.append("\" ");
 		}
-		szCommand.append(QString("%1 %2 ").arg(parts.szHost, parts.iPort));
+		szCommand.append(QString("%1 %2 ").arg(parts.szHost).arg(parts.iPort);
 
 		if(pConsole->connection()) {
 			KviServer* server = pConsole->connection()->target()->server();

src/kvirc/ui/kvi_console.cpp
@@ -307,7 +307,7 @@ void KviConsole::getUserTipText(const QString &nick,KviIrcUserEntry *e,QString &
 
 	if(e->avatar())
 	{
-		buffer += QString("<tr><td><center><img src=\"%1\" width=\"%2\"></center></td></tr>").arg(e->avatar()->localPath(), e->avatar()->size().width());
+		buffer += QString("<tr><td><center><img src=\"%1\" width=\"%2\"></center></td></tr>").arg(e->avatar()->localPath()).arg(e->avatar()->size().width());
 	}
 
 	if(e->hasRealName())

src/kvirc/ui/kvi_userlistview.cpp
@@ -1590,7 +1590,7 @@ void KviUserListView::maybeTip(KviUserListToolTip * pTip, const QPoint & pnt)
 				int iHours = iMins / 60;
 				iMins = iMins % 60;
 				szBuffer += "<tr><td bgcolor=\"#F0F0F0\"><nobr><font color=\"#000000\">";
-				szBuffer += __tr2qs("Quiet for <b>%1h %2m %3s</b>").arg(iHours, iMins, iSecs);
+				szBuffer += __tr2qs("Quiet for <b>%1h %2m %3s</b>").arg(iHours).arg(iMins).arg(iSecs);
 				szBuffer += "</font></nobr></td></tr>";
 			}
 			

src/modules/dcc/send.cpp
@@ -1353,7 +1353,7 @@ QString KviDccFileTransfer::tipText()
 {
 	QString s;
 
-	s = QString("<table><tr><td bgcolor=\"#000000\"><font color=\"#FFFFFF\"><b>DCC %1 (ID %2)</b></font></td></tr>").arg(m_szDccType.ptr(), id());
+	s = QString("<table><tr><td bgcolor=\"#000000\"><font color=\"#FFFFFF\"><b>DCC %1 (ID %2)</b></font></td></tr>").arg(m_szDccType.ptr()).arg(id());
 
 	s += "<tr><td bgcolor=\"#404040\"><font color=\"#FFFFFF\">";
 	s += __tr2qs_ctx("Transfer Log","dcc");

src/modules/options/optw_servers.cpp
@@ -772,7 +772,7 @@ KviServerDetailsWidget::KviServerDetailsWidget(QWidget * par,KviServer * s)
 	KviPointerList<KviProxy> * proxylist = g_pProxyDataBase->proxyList();
 	for(KviProxy * p = proxylist->first();p;p = proxylist->next())
 	{
-		m_pProxyEditor->insertItem(m_pProxyEditor->count(),QString("%1:%2").arg(p->hostname(), p->port()));
+		m_pProxyEditor->insertItem(m_pProxyEditor->count(),QString("%1:%2").arg(p->hostname()).arg(p->port()));
 	}
 	if(m_pProxyEditor->count() > (s->proxy()+2))
 		m_pProxyEditor->setCurrentIndex(s->proxy()+2);