df5abc0ccaadf07d9ca1f6716e1ad718de946a05
Szymon Tomasz Stefanek <pragma@kvirc.net>
2010-07-15 17:09:15 +0000

    Add an explaination for the double indirection macro so people don't loose time searching in C++ forums
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4671 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvilib/config/kvi_debug.h
+++ src/kvilib/config/kvi_debug.h
@@ -110,6 +110,30 @@
 
 	#include "kvi_debugcontext.h"
 
+	// The following two macros are used to create unique variable names
+	// by the means of the __LINE__ builtin macro.
+	// The ## token paste operator must be called inside a macro and must
+	// preceede a macro parameter. This is why we can't use directly
+	//
+	// #define UNIQUEVARIABLE int name ## __LINE__
+	//
+	// We need something like
+	//
+	// #define PASTE(x,y) x ## y
+	// #define UNIQUEVARIABLE int PASTE(x,__LINE__)
+	//
+	// But this doesn't work since the specification of the token pasting operator is
+	//
+	// "If a formal parameter in a macro definition is preceded or followed by the token-pasting operator,
+	// the formal parameter is immediately replaced by the __unexpanded__ actual argument. Macro expansion 
+	// is __not performed__ on the argument prior to replacement."
+	//
+	// So to actually have __LINE__ expanded we need another level of indirection
+	//
+	// #define PASTE(x,y) x ## y
+	// #define EXPAND_Y_AND_THEN_PASTE(x,y) PASTE(x,y)
+	// #define UNIQUEVARIABLE int EXPAND_Y_AND_THEN_PASTE(x,__LINE__)
+
 	#define KVI_TRACE_HACK_TOKENPASTE_2(x,y) x ## y
 	#define KVI_TRACE_HACK_TOKENPASTE_1(x,y) KVI_TRACE_HACK_TOKENPASTE_2(x,y)
 
