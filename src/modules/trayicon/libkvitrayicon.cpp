5c1b0d5b04f233a9adf1c3d8d2171f296385c9b8
Szymon Tomasz Stefanek <pragma@kvirc.net>
2010-08-02 00:53:04 +0000

    Attempt to have both #837 and #872 fixed at the same time. The fix for #837 is reworked as mac specific code.
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4752 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/modules/trayicon/libkvitrayicon.cpp
+++ src/modules/trayicon/libkvitrayicon.cpp
@@ -413,9 +413,24 @@ void KviTrayIcon::refresh()
 
 void KviTrayIcon::activatedSlot( QSystemTrayIcon::ActivationReason reason )
 {
-	if(reason==QSystemTrayIcon::Trigger && (KVI_OPTION_BOOL(KviOption_boolCloseInTray) || !m_pFrm->isVisible()))
+	switch(reason)
 	{
-		toggleParentFrame();
+		case QSystemTrayIcon::Trigger:
+			// This is single click
+#ifdef COMPILE_ON_MAC
+			// On MacOSX one can only single-left-click the icon.
+			// This activates the context menu and is quite confusing if it *also* hides the kvirc window.
+			// So on mac we only _show_ the main window if it's hidden and the CloseInTray option is enabled.
+			if(KVI_OPTION_BOOL(KviOption_boolCloseInTray) && ((!m_pFrm->isVisible()) || m_pFrm->isMinimized()))
+				toggleParentFrame();
+#else //!COMPILE_ON_MAC
+			// on other platforms we always toggle the window
+			toggleParentFrame();
+#endif //!COMPILE_ON_MAC
+		break;
+		default:
+			// we do nothing at this time
+		break;
 	}
 }
 
