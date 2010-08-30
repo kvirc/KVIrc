2ca3febab17f0124a2e52e37ce67e8b0fa1e2d4d
Fabio Bas <ctrlaltca@gmail.com>
2010-08-30 10:35:48 +0000

    fixed switch documentation
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@4945 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/kvs/kvi_kvs_parser_specialcommands.cpp
+++ src/kvirc/kvs/kvi_kvs_parser_specialcommands.cpp
@@ -1337,6 +1337,7 @@ KviKvsTreeNodeCommand * KviKvsParser::parseSpecialCommandSwitch()
 			that terminated the execution with break).[br]
 		@examples:
 			[comment]# Try to change the 1 below to 2 or 3 to see the results[/comment]
+			[example]
 			%tmp = 1
 			switch(%tmp)
 			{
@@ -1350,7 +1351,9 @@ KviKvsTreeNodeCommand * KviKvsParser::parseSpecialCommandSwitch()
 					echo \%tmp was not 1 nor 2: it was %tmp!
 				break;
 			}
+			[/example]
 			[comment]# A complexier example: change the 1 in 2 or 3[/comment]
+			[example]
 			%tmp = 1
 			switch(%tmp)
 			{
@@ -1363,7 +1366,9 @@ KviKvsTreeNodeCommand * KviKvsParser::parseSpecialCommandSwitch()
 					echo \%tmp was either 1 or something different from 2 (%tmp)
 				break;
 			}
+			[/example]
 			[comment]# An example with strings[/comment]
+			[example]
 			%tmp = "This is a test"
 			%tmp2 = "This is not a test"
 			switch(%tmp)
@@ -1386,6 +1391,7 @@ KviKvsTreeNodeCommand * KviKvsParser::parseSpecialCommandSwitch()
 					echo This is executed anyway (unless some break was called)
 				break;
 			}
+			[/example]
 	*/
 
 	if(KVSP_curCharUnicode != '(')
