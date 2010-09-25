kvirc/KVIrc

commit e289064c244e64795bcabd9175773ade0919c968

fixed tray icon notifications, thanks fiox_
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@5028 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvirc/ui/kvi_ircview_gettextline.cpp
@@ -1129,15 +1129,22 @@ void KviIrcView::appendText(int iMsgType,const kvi_wchar_t *data_ptr,int iFlags)
 				m_uLineMarkLineIndex = line_ptr->uIndex;
 				iFlags &= ~SetLineMark;
 			}
-			m_bHaveUnreadedHighlightedMessages = m_bHaveUnreadedHighlightedMessages || iMsgType == KVI_OUT_HIGHLIGHT;
-			m_bHaveUnreadedMessages = m_bHaveUnreadedMessages ||
-				iMsgType == KVI_OUT_CHANPRIVMSG ||
-				iMsgType == KVI_OUT_CHANPRIVMSGCRYPTED ||
-				iMsgType == KVI_OUT_CHANNELNOTICE ||
-				iMsgType == KVI_OUT_CHANNELNOTICECRYPTED ||
-				iMsgType == KVI_OUT_ACTION ||
-				iMsgType == KVI_OUT_OWNPRIVMSGCRYPTED ||
-				iMsgType == KVI_OUT_HIGHLIGHT;
 		}
+		
+		if(!m_bHaveUnreadedHighlightedMessages && iMsgType == KVI_OUT_HIGHLIGHT)
+			m_bHaveUnreadedHighlightedMessages = true;
+		if(!m_bHaveUnreadedMessages && (
+			iMsgType == KVI_OUT_CHANPRIVMSG ||
+			iMsgType == KVI_OUT_CHANPRIVMSGCRYPTED ||
+			iMsgType == KVI_OUT_CHANNELNOTICE ||
+			iMsgType == KVI_OUT_CHANNELNOTICECRYPTED ||
+			iMsgType == KVI_OUT_ACTION ||
+			iMsgType == KVI_OUT_QUERYPRIVMSG ||
+			iMsgType == KVI_OUT_QUERYPRIVMSGCRYPTED ||
+			iMsgType == KVI_OUT_DCCCHATMSG ||
+			iMsgType == KVI_OUT_DCCCHATMSGCRYPTED ||
+			iMsgType == KVI_OUT_HIGHLIGHT
+		)) 
+			m_bHaveUnreadedMessages = true;
 	}
 }

commit 826dc1ed35352bb8fb6c2d4207433f160d714f04

fixed a regression in tray notifications introduced in r5028, thanks Arfrever
git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@5031 17fca916-40b9-46aa-a4ea-0a15b648b75c

src/kvirc/ui/kvi_ircview.h
@@ -151,7 +151,7 @@ public:
 	void enableDnd(bool bEnable);
 	bool haveUnreadedMessages() { return m_bHaveUnreadedMessages; };
 	bool haveUnreadedHighlightedMessages() { return m_bHaveUnreadedHighlightedMessages; };
-	enum AppendTextFlags { NoRepaint = 1, NoTimestamp = 2, SetLineMark = 4 };
+	enum AppendTextFlags { NoRepaint = 1, NoTimestamp = 2, SetLineMark = 4, TriggersNotification = 8 };
 	void appendText(int msg_type,const kvi_wchar_t *data_ptr,int iFlags = 0);
 	void clearLineMark(bool bRepaint=false);
 	bool hasLineMark(){ return m_uLineMarkLineIndex != KVI_IRCVIEW_INVALID_LINE_MARK_INDEX; };

src/kvirc/ui/kvi_ircview_gettextline.cpp
@@ -1131,20 +1131,23 @@ void KviIrcView::appendText(int iMsgType,const kvi_wchar_t *data_ptr,int iFlags)
 			}
 		}
 		
-		if(!m_bHaveUnreadedHighlightedMessages && iMsgType == KVI_OUT_HIGHLIGHT)
-			m_bHaveUnreadedHighlightedMessages = true;
-		if(!m_bHaveUnreadedMessages && (
-			iMsgType == KVI_OUT_CHANPRIVMSG ||
-			iMsgType == KVI_OUT_CHANPRIVMSGCRYPTED ||
-			iMsgType == KVI_OUT_CHANNELNOTICE ||
-			iMsgType == KVI_OUT_CHANNELNOTICECRYPTED ||
-			iMsgType == KVI_OUT_ACTION ||
-			iMsgType == KVI_OUT_QUERYPRIVMSG ||
-			iMsgType == KVI_OUT_QUERYPRIVMSGCRYPTED ||
-			iMsgType == KVI_OUT_DCCCHATMSG ||
-			iMsgType == KVI_OUT_DCCCHATMSGCRYPTED ||
-			iMsgType == KVI_OUT_HIGHLIGHT
-		)) 
-			m_bHaveUnreadedMessages = true;
+		if(iFlags & TriggersNotification)
+		{
+			if(!m_bHaveUnreadedHighlightedMessages && iMsgType == KVI_OUT_HIGHLIGHT)
+				m_bHaveUnreadedHighlightedMessages = true;
+			if(!m_bHaveUnreadedMessages && (
+				iMsgType == KVI_OUT_CHANPRIVMSG ||
+				iMsgType == KVI_OUT_CHANPRIVMSGCRYPTED ||
+				iMsgType == KVI_OUT_CHANNELNOTICE ||
+				iMsgType == KVI_OUT_CHANNELNOTICECRYPTED ||
+				iMsgType == KVI_OUT_ACTION ||
+				iMsgType == KVI_OUT_QUERYPRIVMSG ||
+				iMsgType == KVI_OUT_QUERYPRIVMSGCRYPTED ||
+				iMsgType == KVI_OUT_DCCCHATMSG ||
+				iMsgType == KVI_OUT_DCCCHATMSGCRYPTED ||
+				iMsgType == KVI_OUT_HIGHLIGHT
+			)) 
+				m_bHaveUnreadedMessages = true;
+		}
 	}
 }

src/kvirc/ui/kvi_window.cpp
@@ -1227,6 +1227,7 @@ void KviWindow::internalOutput(KviIrcView * pView,int msg_type,const kvi_wchar_t
 	{
 		if((this != g_pActiveWindow) || (!isActiveWindow()))
 		{
+			iFlags |= KviIrcView::TriggersNotification;
 			if(!pView->hasLineMark())
 			{
 				iFlags |= KviIrcView::SetLineMark;