518259b0c7a5fece17d726598d4714c1fc5b7733
Fabio Bas <ctrlaltca@gmail.com>
2010-12-23 14:22:52 +0000

    boolDontShowNotifierIfActiveWindowIsFullScreen true by default (#1056)
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@5223 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/kernel/kvi_options.cpp
+++ src/kvirc/kernel/kvi_options.cpp
@@ -325,7 +325,7 @@ KviBoolOption g_boolOptionsTable[KVI_NUM_BOOL_OPTIONS]=
 	BOOL_OPTION("UseSaslIfAvailable",true,KviOption_sectFlagConnection),
 	BOOL_OPTION("FrameIsMaximized",false,KviOption_sectFlagGeometry),
 	BOOL_OPTION("PrependNickColorInfoToRealname",true,KviOption_sectFlagConnection),
-	BOOL_OPTION("DontShowNotifierIfActiveWindowIsFullScreen",false,KviOption_sectFlagFrame),
+	BOOL_OPTION("DontShowNotifierIfActiveWindowIsFullScreen",true,KviOption_sectFlagFrame),
 	BOOL_OPTION("WheelScrollsWindowsList",true,KviOption_sectFlagWindowList),
 	BOOL_OPTION("AcceptMismatchedPortDccResumeRequests",false,KviOption_sectFlagDcc),
 	BOOL_OPTION("EnableUrlLinkToolTip",true,KviOption_sectFlagGui),
