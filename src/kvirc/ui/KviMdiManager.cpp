ccb31dd1fb6483c8299cddcdbc7c74efb2a9d760
Fabio Bas <ctrlaltca@gmail.com>
2011-02-02 22:34:56 +0000

    reverted part of #5411, it lead to an infinite focus recursion (backtrace: http://pastebin.com/Fg54w6dG)
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@5415 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/ui/KviMdiManager.cpp
+++ src/kvirc/ui/KviMdiManager.cpp
@@ -150,12 +150,6 @@ KviMdiChild * KviMdiManager::topChild()
 void KviMdiManager::setTopChild(KviMdiChild *lpC)
 {
 	setActiveSubWindow((QMdiSubWindow*) lpC);
-	if(!lpC->isVisible())
-		lpC->show();
-	lpC->raise();
-	if(!lpC->hasFocus())
-		lpC->setFocus();
-	//qDebug("Set top child %x: visible %d has focus %d geometry %d,%d,%d,%d",lpC,lpC->isVisible(),lpC->hasFocus(),lpC->geometry().x(),lpC->geometry().y(),lpC->geometry().width(),lpC->geometry().height());
 }
 
 void KviMdiManager::hideChild(KviMdiChild *lpC)
