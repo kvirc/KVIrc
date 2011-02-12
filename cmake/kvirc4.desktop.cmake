164a1023b0130dc47eabf76ccf8883cfbe53bcd2
Kai Wasserbäch <debian@carbon-project.org>
2011-02-12 15:30:26 +0000

    kvirc.{desktop,notifiyrc}: Added German translations.
    Signed-off-by: Kai Wasserbäch <curan@debian.org>
    
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@5471 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- cmake/kvirc4.desktop.cmake
+++ cmake/kvirc4.desktop.cmake
@@ -1,10 +1,12 @@
 [Desktop Entry]
 Name=KVIrc 4
 GenericName=IRC Client
+GenericName[de]=IRC-Client
 GenericName[pl]=Klient IRC
 GenericName[pt]=Cliente IRC
 GenericName[tr]=IRC İstemcisi
 Comment=Connect to Internet Relay Chat
+Comment[de]=Internet Relay Chat betreten.
 Comment[pl]=Połącz się z Internet Relay Chat (IRC)
 Comment[pt]=Conectar ao IRC (Internet Relay Chat)
 Comment[tr]=IRC'e bağlanın
