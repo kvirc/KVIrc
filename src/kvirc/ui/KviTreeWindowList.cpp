9f68a6196d5d5fb90a1eb6268057c6b77d0515a0
Fabio Bas <ctrlaltca@gmail.com>
2011-03-20 16:45:01 +0000

    fix for #1116
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@5651 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/ui/KviTreeWindowList.cpp
+++ src/kvirc/ui/KviTreeWindowList.cpp
@@ -246,29 +246,23 @@ void KviTreeWindowListTreeWidget::mousePressEvent(QMouseEvent *e)
 				//shitf+left click: close window
 				wnd->delayedClose();
 			} else {
+				//left click activate/deactivate window
+				if((g_pActiveWindow != wnd) || (wnd->isMinimized()))
+					g_pMainWindow->setActiveWindow(wnd);
+				else 
+					wnd->minimize();
+
 				// let our parent handle clicks on branches, etc
 				QStyleOption option;
 				option.initFrom(this);
 				option.rect = visualItemRect(it);
 				if(!style()->subElementRect(QStyle::SE_TreeViewDisclosureItem, &option, this).contains(e->pos()))
-				{
 					QTreeWidget::mousePressEvent(e);
-					return;
-				}
-
-				//left click activate/deactivate window
-				if((g_pActiveWindow != wnd) || (wnd->isMinimized()))
-				{
-					g_pMainWindow->setActiveWindow(wnd);
-				} else wnd->minimize();
 			}
-
-		} else if(e->button() & Qt::RightButton)
-		{
+		} else if(e->button() & Qt::RightButton) {
 			//right click: open popup
 			wnd->contextPopup();
-		}else if(e->button() & Qt::MidButton)
-		{
+		} else if(e->button() & Qt::MidButton) {
 			//mid click: close window
 			wnd->delayedClose();
  		}
