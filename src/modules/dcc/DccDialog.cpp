897f6cb92f4ba162cffc0fc59c8e4bdc0a839cf7
Fabio Bas <ctrlaltca@gmail.com>
2011-07-11 22:23:56 +0000

    fix for #1168
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@5893 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/modules/dcc/DccDialog.cpp
+++ src/modules/dcc/DccDialog.cpp
@@ -183,8 +183,8 @@ void DccRenameDialog::closeEvent(QCloseEvent *e)
 
 void DccRenameDialog::showEvent(QShowEvent *e)
 {
-	move((g_pApp->desktop()->width() - width()) >> 1,
-		(g_pApp->desktop()->height() - height()) >> 1);
+	QRect rect = g_pApp->desktop()->screenGeometry(g_pApp->desktop()->primaryScreen());
+	move((rect.width() - width())/2,(rect.height() - height())/2);
 	QWidget::showEvent(e);
 }
 
