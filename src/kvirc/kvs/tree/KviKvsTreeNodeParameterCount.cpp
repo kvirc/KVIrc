1a91d6a0be06f020750abd87aa203ec74494a5a1
Szymon Tomasz Stefanek <pragma@kvirc.net>
2011-12-21 05:23:39 +0000

    Make $# work again (add missing files)... and this is commit 6000 :)
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@6000 17fca916-40b9-46aa-a4ea-0a15b648b75c

---
+++ src/kvirc/kvs/tree/KviKvsTreeNodeParameterCount.cpp
@@ -0,0 +1,58 @@
+//=============================================================================
+//
+//   File : KviKvsTreeNodeParameterCount.cpp
+//   Creation date : Tue 07 Oct 2003 03:47:11 by Szymon Stefanek
+//
+//   This file is part of the KVIrc IRC client distribution
+//   Copyright (C) 2003-2010 Szymon Stefanek <pragma at kvirc dot net>
+//
+//   This program is FREE software. You can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your opinion) any later version.
+//
+//   This program is distributed in the HOPE that it will be USEFUL,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, write to the Free Software Foundation,
+//   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+//=============================================================================
+
+#include "KviKvsTreeNodeParameterCount.h"
+#include "KviKvsRunTimeContext.h"
+#include "KviKvsVariant.h"
+
+KviKvsTreeNodeParameterCount::KviKvsTreeNodeParameterCount(const QChar * pLocation)
+: KviKvsTreeNodeData(pLocation)
+{
+}
+
+KviKvsTreeNodeParameterCount::~KviKvsTreeNodeParameterCount()
+{
+}
+
+void KviKvsTreeNodeParameterCount::contextDescription(QString &szBuffer)
+{
+	szBuffer = "ParameterCount \"$#\"";
+}
+
+void KviKvsTreeNodeParameterCount::dump(const char * prefix)
+{
+	qDebug("%s ParameterCount",prefix);
+}
+
+bool KviKvsTreeNodeParameterCount::canEvaluateToObjectReference()
+{
+	return true;
+}
+
+bool KviKvsTreeNodeParameterCount::evaluateReadOnly(KviKvsRunTimeContext * c,KviKvsVariant * pBuffer)
+{
+	pBuffer->setInteger(c->parameterList()->count());
+	return true;
+}
+
---
+++ src/kvirc/kvs/tree/KviKvsTreeNodeParameterCount.h
@@ -0,0 +1,46 @@
+#ifndef _KVI_KVS_TREENODE_PARAMETERCOUNT_H_
+#define _KVI_KVS_TREENODE_PARAMETERCOUNT_H_
+//=============================================================================
+//
+//   File : KviKvsTreeNodeParameterCount.h
+//   Creation date : Thu 21 Dec 2011 06:13:11 by Szymon Tomasz Stefanek
+//
+//   This file is part of the KVIrc IRC client distribution
+//   Copyright (C) 2011 Szymon Stefanek <pragma at kvirc dot net>
+//
+//   This program is FREE software. You can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your opinion) any later version.
+//
+//   This program is distributed in the HOPE that it will be USEFUL,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, write to the Free Software Foundation,
+//   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+//=============================================================================
+
+#include "kvi_settings.h"
+#include "KviKvsTreeNodeData.h"
+
+class KviKvsRunTimeContext;
+class KviKvsVariant;
+
+class KVIRC_API KviKvsTreeNodeParameterCount : public KviKvsTreeNodeData
+{
+public:
+	KviKvsTreeNodeParameterCount(const QChar * pLocation);
+	~KviKvsTreeNodeParameterCount();
+public:
+	virtual void contextDescription(QString &szBuffer);
+	virtual void dump(const char * prefix);
+
+	virtual bool canEvaluateToObjectReference();
+	virtual bool evaluateReadOnly(KviKvsRunTimeContext * c,KviKvsVariant * pBuffer);
+};
+
+#endif //!_KVI_KVS_TREENODE_PARAMETERCOUNT_H_
