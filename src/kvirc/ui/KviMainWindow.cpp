f63113c4b3eac4fd7d3446bac9b9ecf76cc93493
Fabio Bas <ctrlaltca@gmail.com>
2012-04-08 16:52:28 +0000

    enforce active window when mainwindow gets activated; should fix a problem where windows created when kvirc was minimized/hidden appears not-maximized on top of the others
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@6127 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvirc/ui/KviMainWindow.cpp
+++ src/kvirc/ui/KviMainWindow.cpp
@@ -803,7 +803,11 @@ void KviMainWindow::changeEvent(QEvent * e)
 		if(isActiveWindow())
 		{
 			if(g_pActiveWindow)
+			{
+				qDebug("KviMainWindow::changeEvent g_pActiveWindow=%s", g_pActiveWindow->windowName().toUtf8().data());
+				setActiveWindow(g_pActiveWindow);
 				childWindowActivated(g_pActiveWindow, true);
+			}
 		} else {
 			if(g_pActiveWindow)
 				g_pActiveWindow->lostUserFocus();
