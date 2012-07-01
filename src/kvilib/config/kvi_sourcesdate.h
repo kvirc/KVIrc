39f74c8619c7900becc1af07c00cf8fa3935f374
Fabio Bas <ctrlaltca@gmail.com>
2012-07-01 12:59:03 +0000

    updated Changelog, hackers.guide.txt, sourcesdate version bump: 4.2.0 added a stub README entry for 4.2.0
    git-svn-id: https://svn.kvirc.de/svn/trunk/kvirc@6187 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvilib/config/kvi_sourcesdate.h
+++ src/kvilib/config/kvi_sourcesdate.h
@@ -36,8 +36,8 @@
 * \def KVI_SOURCES_DATE Defines the sources date
 * \def KVI_SOURCES_DATE_NUMERIC Defines the sources date
 */
-#define KVI_SOURCES_DATE "20120416"
-#define KVI_SOURCES_DATE_NUMERIC 0x20120416
+#define KVI_SOURCES_DATE "20120701"
+#define KVI_SOURCES_DATE_NUMERIC 0x20120701
 
 /**
 * \brief This is the date of the sources before that we should force a setup
