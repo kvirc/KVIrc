545cb367fa0de911ca92335f8cf20955e933e806
Szymon Tomasz Stefanek <s.stefanek@gmail.com>
2015-09-28 01:39:34 +0200

    Fix input background image.

--- src/kvirc/ui/KviInputEditor.cpp
+++ src/kvirc/ui/KviInputEditor.cpp
@@ -308,7 +308,7 @@ void KviInputEditor::paintEvent(QPaintEvent *)
 #endif
 		p.fillRect(cr,KVI_OPTION_COLOR(KviOption_colorInputBackground));
 
-		QPixmap * pix = KVI_OPTION_PIXMAP(KviOption_pixmapLabelBackground).pixmap();
+		QPixmap * pix = KVI_OPTION_PIXMAP(KviOption_pixmapInputBackground).pixmap();
 		if(pix)
 			KviPixmapUtils::drawPixmapWithPainter(&p,pix,KVI_OPTION_UINT(KviOption_uintTreeWindowListPixmapAlign),cr,cr.width(),cr.height());
 #ifdef COMPILE_PSEUDO_TRANSPARENCY
