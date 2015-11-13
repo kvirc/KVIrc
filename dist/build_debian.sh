41498d07219e6d50d341b1989aca24d908a449f2
Alexander Pozdnyakov <almipo@mail.ru>
2015-11-13 21:31:17 +0300

    add debian rules

---
+++ dist/build_debian.sh
@@ -0,0 +1,37 @@
+#/bin/sh
+
+export DEBFULLNAME="Alexander Pozdnyakov"
+export DEBEMAIL="almipo@mail.ru"
+
+DIR=$(pwd)
+BUILDDIR="${DIR}/build"
+cd .. && GITDIR=$(pwd)
+PKG_NAME=kvirc
+SVNGITBZR="~"
+VERSION='4:4.3.2'
+VERSION1='4.3.2'
+
+test -d $BUILDDIR && rm -rf ${BUILDDIR} 
+mkdir -p $BUILDDIR
+
+cd $GITDIR
+
+dat=$(git describe --dirty)
+branch=$(git branch | grep "\*" | sed 's/\* //g')
+commit=$(git log -1 | grep -i "^commit" | awk '{print $2}')
+datct=$(git log -n 1 --format=%ct)
+
+tar -cpf  "${BUILDDIR}/${PKG_NAME}_${VERSION1}${SVNGITBZR}${dat}.orig.tar" --exclude ".git" --exclude "dist" . 
+cd ${BUILDDIR}
+xz -9 "${PKG_NAME}_${VERSION1}${SVNGITBZR}${dat}.orig.tar"
+mkdir "${PKG_NAME}-${VERSION1}${SVNGITBZR}${dat}"
+tar -xpJf "${PKG_NAME}_${VERSION1}${SVNGITBZR}${dat}.orig.tar.xz" -C "${PKG_NAME}-${VERSION1}${SVNGITBZR}${dat}"
+cd $DIR
+cp -r debian $BUILDDIR/${PKG_NAME}-${VERSION1}${SVNGITBZR}${dat}/debian
+cd $BUILDDIR/${PKG_NAME}-${VERSION1}${SVNGITBZR}${dat}/
+dch -v "${VERSION}${SVNGITBZR}${dat}-1" -D "unstable" --force-distribution "Compile"
+dch -a "Branch: $branch"
+dch -a "Commit: $commit"
+dch -a "Date: $datct"
+
+debuild
