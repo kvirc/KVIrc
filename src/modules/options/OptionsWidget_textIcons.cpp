4a515900c510011224aab533a5bb9a9891034e29
wodim <neikokz@gmail.com>
2015-11-17 20:49:12 +0100

    When adding a new text icon, ensure a theme directory in the user config directory exists already.
    Fixes #1747.

--- src/modules/options/OptionsWidget_textIcons.cpp
+++ src/modules/options/OptionsWidget_textIcons.cpp
@@ -205,6 +205,8 @@ void OptionsWidget_textIcons::chooseFromFile()
 			g_pApp->getLocalKvircDirectory(szCurrentThemePath,KviApplication::Themes,KVI_OPTION_STRING(KviOption_stringIconThemeSubdir));
 			szCurrentThemePath+= KVI_PATH_SEPARATOR_CHAR;
 			qDebug("copy source %s - dest %s",szFile.toUtf8().data(),szCurrentThemePath.toUtf8().data());
+			if (!KviFileUtils::directoryExists(szCurrentThemePath+szFileName))
+				KviFileUtils::makeDir(szCurrentThemePath);
 			KviFileUtils::copyFile(szFile,szCurrentThemePath+szFileName);
 			m_pCurrentItem->icon()->setFilename(szFileName);
 			qDebug("set Icon");
