eef27587f6181e7c87fd973f11b3dd77e5708c41
un1versal <un1versal@users.noreply.github.com>
2015-12-10 13:54:47 +0000

    AddonFunctions: fixing window title to something saner, thx to xrogaan

--- src/modules/addon/AddonFunctions.cpp
+++ src/modules/addon/AddonFunctions.cpp
@@ -241,7 +241,7 @@ namespace AddonFunctions
 		}
 
 		if(!KviFileUtils::deleteDir(szUnpackPath))
-			QMessageBox::warning(NULL,__tr2qs_ctx("Addon Unpack Warning","addon"),__tr2qs_ctx("Failed to delete the directory '%1'","addon").arg(szUnpackPath));
+			QMessageBox::warning(NULL,__tr2qs_ctx("Warning While Unpacking Addon","addon"),__tr2qs_ctx("Failed to delete the directory '%1'","addon").arg(szUnpackPath));
 
 		return true;
 	}
