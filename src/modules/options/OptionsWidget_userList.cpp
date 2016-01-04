7cd7891ddfd2ff99f603f6955cd36fcbdd8830c0
un1versal <un1versal@users.noreply.github.com>
2016-01-04 18:59:51 +0000

    append (blend color) to string entry to reflect what it actually is.
    closes #1814
    
    https://github.com/kvirc/KVIrc/issues/1814#issuecomment-168728737

--- src/modules/options/OptionsWidget_userList.cpp
+++ src/modules/options/OptionsWidget_userList.cpp
@@ -64,7 +64,7 @@ OptionsWidget_userListForeground::OptionsWidget_userListForeground(QWidget * par
 
 	KviTalHBox * ahb = new KviTalHBox(g);
 	ahb->setSpacing(4);
-	KviBoolSelector * ab = addBoolSelector(ahb,__tr2qs_ctx("Away:","options"),KviOption_boolUserListViewUseAwayColor);
+	KviBoolSelector * ab = addBoolSelector(ahb,__tr2qs_ctx("Away (blend color):","options"),KviOption_boolUserListViewUseAwayColor);
 	KviColorSelector * as = addColorSelector(ahb,QString(),KviOption_colorUserListViewAwayForeground);
 	connect(ab,SIGNAL(toggled(bool)),as,SLOT(setEnabled(bool)));
 
