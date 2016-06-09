kvirc/KVIrc

commit 28e4927ba163fd992035f1ebe8638cddbb15c671

Option to hide the menu bar temporarily.

src/kvirc/kernel/KviCoreActions.cpp
@@ -442,7 +442,7 @@ void register_core_actions(KviActionManager * m)
 		"kvi_bigicon_quitapp.png",
 		KviIconManager::QuitApp,
 		0,
-		QString());
+		KVI_SHORTCUTS_QUIT);
 }
 
 

src/kvilib/config/kvi_shortcuts.h
@@ -46,6 +46,7 @@
 * \def KVI_SHORTCUTS_OPTIONS Open "Options" dialog
 * \def KVI_SHORTCUTS_SERVERS Open "Servers" dialog
 * \def KVI_SHORTCUTS_THEME Open "Theme Options" dialog
+* \def KVI_SHORTCUTS_TOGGLE_MENU_BAR Toggle visibility of the menu bar
 * \def KVI_SHORTCUTS_USERS Open "Registered Users" dialog
 * \def KVI_SHORTCUTS_MANAGE_THEMES Open "Manage Theme" dialog
 * \def KVI_SHORTCUTS_MANAGE_ADDONS Open "Manage Theme" dialog
@@ -136,10 +137,15 @@
 #define KVI_SHORTCUTS_JOIN "Ctrl+J"
 #define KVI_SHORTCUTS_INPUT_COLOR "Ctrl+K"
 #define KVI_SHORTCUTS_WIN_SCROLL_TO_LAST_READ_LINE "Ctrl+L"
+#define KVI_SHORTCUTS_TOGGLE_MENU_BAR "Ctrl+M"
 #define KVI_SHORTCUTS_CONTEXT QKeySequence::New					// Ctrl+N
 #define KVI_SHORTCUTS_INPUT_RESET "Ctrl+O"
 #define KVI_SHORTCUTS_INPUT_PLAINTEXT "Ctrl+P"
-#define KVI_SHORTCUTS_QUIT "Ctrl+Q"
+#ifdef COMPILE_ON_MAC
+# define KVI_SHORTCUTS_QUIT QString()
+#else
+# define KVI_SHORTCUTS_QUIT "Ctrl+Q"
+#endif
 #define KVI_SHORTCUTS_INPUT_REVERSE "Ctrl+R"
 #define KVI_SHORTCUTS_SERVERS "Ctrl+S"
 #define KVI_SHORTCUTS_INPUT_UNDERLINE QKeySequence::Underline			// Ctrl+U

src/kvirc/kernel/KviOptions.cpp
@@ -338,6 +338,7 @@ KviBoolOption g_boolOptionsTable[KVI_NUM_BOOL_OPTIONS]=
 	BOOL_OPTION("RequireControlToCopy",false,KviOption_sectFlagIrcView),
 	BOOL_OPTION("Send64BitAckInDccRecv",false,KviOption_sectFlagDcc),
 	BOOL_OPTION("ShowTreeWindowListHandle",true,KviOption_sectFlagWindowList | KviOption_resetUpdateGui | KviOption_groupTheme),
+	BOOL_OPTION("MenuBarVisible",true,KviOption_sectFlagFrame | KviOption_resetUpdateGui),
 };
 
 #define STRING_OPTION(_txt,_val,_flags) KviStringOption(KVI_STRING_OPTIONS_PREFIX _txt,_val,_flags)

src/kvirc/kernel/KviOptions.h
@@ -358,8 +358,9 @@ DECLARE_OPTION_STRUCT(KviStringListOption,QStringList)
 #define KviOption_boolRequireControlToCopy 263                           /* interface::ircview */
 #define KviOption_boolSend64BitAckInDccRecv 264                          /* dcc::send */
 #define KviOption_boolShowTreeWindowListHandle 265
+#define KviOption_boolMenuBarVisible 266
 
-#define KVI_NUM_BOOL_OPTIONS 266
+#define KVI_NUM_BOOL_OPTIONS 267
 
 
 #define KVI_STRING_OPTIONS_PREFIX "string"

src/kvirc/ui/KviMainWindow.cpp
@@ -133,6 +133,9 @@ KviMainWindow::KviMainWindow()
     m_pAccellerators = new KviPointerList<QShortcut>;
 	m_pMenuBar   = new KviMenuBar(this,"main_menu_bar");
 	setMenuWidget(m_pMenuBar);
+#ifndef COMPILE_ON_MAC
+	m_pMenuBar->setVisible(KVI_OPTION_BOOL(KviOption_boolMenuBarVisible));
+#endif
 
 	if(KVI_OPTION_BOOL(KviOption_boolStatusBarVisible))
 	{
@@ -965,6 +968,10 @@ void KviMainWindow::applyOptions()
 
 	m_pWindowList->applyOptions();
 	g_pTextIconManager->applyOptions();
+
+#ifndef COMPILE_ON_MAC
+	m_pMenuBar->setVisible(KVI_OPTION_BOOL(KviOption_boolMenuBarVisible));
+#endif
 }
 
 void KviMainWindow::toggleStatusBar()
@@ -978,6 +985,18 @@ void KviMainWindow::toggleStatusBar()
 		m_pStatusBar->load();
 		setStatusBar(m_pStatusBar);
 		m_pStatusBar->show();
+	}
+}
+
+void KviMainWindow::toggleMenuBar()
+{
+	if(KVI_OPTION_BOOL(KviOption_boolMenuBarVisible) == true)
+	{
+		m_pMenuBar->hide();
+		KVI_OPTION_BOOL(KviOption_boolMenuBarVisible) = false;
+	} else {
+		m_pMenuBar->show();
+		KVI_OPTION_BOOL(KviOption_boolMenuBarVisible) = true;
 	}
 }
 

src/kvirc/ui/KviMainWindow.h
@@ -159,6 +159,7 @@ public slots:
 	void newConsole();
 	void executeInternalCommand(int index);
 	void toggleStatusBar();
+	void toggleMenuBar();
 	void customizeToolBars();
 protected:
 	void restoreModuleExtensionToolBars();

src/kvirc/ui/KviMenuBar.cpp
@@ -41,6 +41,7 @@
 #include "KviKvsScript.h"
 #include "KviShortcut.h"
 
+#include <QKeySequence>
 #include <QMenu>
 
 KviMenuBar::KviMenuBar(KviMainWindow * par,const char * name)
@@ -164,6 +165,7 @@ void KviMenuBar::actionTriggered(bool)
 void KviMenuBar::updateSettingsPopup()
 {
 	m_pStatusBarAction->setChecked(m_pFrm->mainStatusBar());
+	m_pMenuBarAction->setChecked(true);
 }
 
 void KviMenuBar::setupSettingsPopup(QMenu *pop)
@@ -173,6 +175,14 @@ void KviMenuBar::setupSettingsPopup(QMenu *pop)
 
 	QAction *pAction = opt->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Toolbar)),__tr2qs("Toolbars"));
 	pAction->setMenu(m_pToolbarsPopup);
+
+#ifndef COMPILE_ON_MAC
+	m_pMenuBarAction = opt->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::StatusBar)),__tr2qs("Show Menu Bar"),m_pFrm,SLOT(toggleMenuBar()));
+	// must be added to the main window or it stops working when the menu is hidden :/
+	g_pMainWindow->addAction(m_pMenuBarAction);
+	m_pMenuBarAction->setCheckable(true);
+	m_pMenuBarAction->setShortcut(QKeySequence::fromString(KVI_SHORTCUTS_TOGGLE_MENU_BAR));
+#endif
 
 	m_pStatusBarAction = opt->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::StatusBar)),__tr2qs("Show Status Bar"),m_pFrm,SLOT(toggleStatusBar()));
 	m_pStatusBarAction->setCheckable(true);
@@ -240,21 +250,13 @@ void KviMenuBar::setupMainPopup(QMenu *pop)
 	m_pDisconnectAction = main->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Quit)),__tr2qs("Disconnect"),this,SLOT(actionTriggered(bool)));
 	m_pDisconnectAction->setData(KVI_INTERNALCOMMAND_QUIT);
 
-	main->addSeparator();
-
 	// FIXME: Add a "Dock to tray" icon if the tray is not visible (or show tray icon or whatever)
 
 // Qt/Mac creates a Quit item on its own <= this is bad
 #ifndef COMPILE_ON_MAC
 	main->addSeparator();
 
-	main->addAction(
-			*(g_pIconManager->getSmallIcon(KviIconManager::QuitApp)),
-			__tr2qs("&Quit"),
-			g_pMainWindow,
-			SLOT(close()),
-			QKeySequence(KVI_SHORTCUTS_QUIT)
-		);
+	ACTION_POPUP_ITEM(KVI_COREACTION_QUITKVIRC,main)
 #endif //COMPILE_ON_MAC
 }
 

src/kvirc/ui/KviMenuBar.h
@@ -56,6 +56,7 @@ protected:
 	QMenu                       * m_pActionsToolsPopup;
 	KviPointerList<KviScriptMenuBarItem> * m_pScriptItemList;
     // Dynamic actions
+    QAction                     * m_pMenuBarAction;
     QAction                     * m_pStatusBarAction;
     QAction                     * m_pDisconnectAction;
 	QAction                     * m_pModulesToolsAction;

commit bf3bcb423efef8c03e9917ba83cacab96f30faa3

KviCoreActions: fix confirmation on quit
fix quit message when boolConfirmCloseWhenThereAreConnections is
enabled.

broken since 28e4927

fixes #2006

src/kvirc/kernel/KviCoreActions.cpp
@@ -431,9 +431,9 @@ void register_core_actions(KviActionManager * m)
 
 	SLOT_ACTION(
 	    KVI_COREACTION_QUITKVIRC,
-	    g_pApp,
-	    SLOT(quit()),
-	    __tr2qs("Quit KVIrc"),
+	    g_pMainWindow,
+	    SLOT(close()),
+	    __tr2qs("&Quit KVIrc"),
 	    __tr2qs("Quits KVIrc closing all the current connections"),
 	    KviActionManager::categoryGeneric(),
 	    "kvi_bigicon_quitapp.png",