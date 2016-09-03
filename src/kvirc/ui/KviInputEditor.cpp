030549610a1b227b05046cc6478822ae1eaf97b6
IceN9ne <IceN9ne.code@gmail.com>
2016-09-03 18:32:18 -0400

    KviInputEditor: Make the lambda predicate easer to read

--- src/kvirc/ui/KviInputEditor.cpp
+++ src/kvirc/ui/KviInputEditor.cpp
@@ -2300,9 +2300,11 @@ void KviInputEditor::completion(bool bShift)
 		{
 			QString szAll;
 			szMatch = tmp.front();
-			auto predicate = bIsDir
-				? std::function<bool(const QChar &, const QChar &)>([](const QChar & a, const QChar & b) { return a.unicode() == b.unicode(); })
-				: std::function<bool(const QChar &, const QChar &)>([](const QChar & a, const QChar & b) { return a.toLower().unicode() == b.toLower().unicode(); });
+			auto predicate =
+				[bIsDir](const QChar & a, const QChar & b)
+				{
+					return bIsDir ? (a.unicode() == b.unicode()) : (a.toLower().unicode() == b.toLower().unicode());
+				};
 
 			for(auto szTmp : tmp)
 			{
