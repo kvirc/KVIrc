21e4e7490f1922d298c2cfb7520a5f5a3508097b
un1versal <un1versal@users.noreply.github.com>
2016-09-15 00:44:30 +0100

    oops! slipped into comma

--- src/kvirc/kernel/KviOptions.cpp
+++ src/kvirc/kernel/KviOptions.cpp
@@ -69,7 +69,7 @@ KviBoolOption g_boolOptionsTable[KVI_NUM_BOOL_OPTIONS] = {
 	BOOL_OPTION("InputHistoryCursorAtEnd", true, KviOption_sectFlagInput),
 	BOOL_OPTION("AvoidParserWarnings", false, KviOption_sectFlagUserParser),
 	BOOL_OPTION("UseProxyHost", false, KviOption_sectFlagConnection),
-	BOOL_OPTION("IgnoreSpecialCharactersInNickCompletion", false, KviOption_sectFlagInput)
+	BOOL_OPTION("IgnoreSpecialCharactersInNickCompletion", false, KviOption_sectFlagInput),
 	BOOL_OPTION("ForceBrutalQuit", false, KviOption_sectFlagIrcSocket),
 	BOOL_OPTION("ShowPingPong", true, KviOption_sectFlagConnection),
 	BOOL_OPTION("PopupNotifierOnNewQueryMessages", true, KviOption_sectFlagFrame),
@@ -332,7 +332,7 @@ KviBoolOption g_boolOptionsTable[KVI_NUM_BOOL_OPTIONS] = {
 	BOOL_OPTION("ShowTreeWindowListHandle", true, KviOption_sectFlagWindowList | KviOption_resetUpdateGui | KviOption_groupTheme),
 	BOOL_OPTION("MenuBarVisible", true, KviOption_sectFlagFrame | KviOption_resetUpdateGui),
 	BOOL_OPTION("WarnAboutHidingMenuBar", true, KviOption_sectFlagFrame),
-	BOOL_OPTION("WhoRepliesToActiveWindow", false, KviOption_sectFlagConnection),
+	BOOL_OPTION("WhoRepliesToActiveWindow", false, KviOption_sectFlagConnection)
 };
 
 // NOTICE: REUSE EQUIVALENT UNUSED KviOption_bool in KviOptions.h ENTRIES BEFORE ADDING NEW ENTRIES ABOVE
