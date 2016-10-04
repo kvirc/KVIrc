9a40bfa3696f73cbb440917942b9def37a84ae50
Dessa <Dessa@gmake.de>
2016-10-04 15:05:05 +0200

    build unicode aware installers, that would have even worked on xp but was somewhat forgotten back in the day

--- cmake/KVIrc.nsi.cmake
+++ cmake/KVIrc.nsi.cmake
@@ -21,6 +21,7 @@ OutFile KVIrc-${VERSION}-dev-${GIT_VERSION}.exe
 ; Stable version, e.g. KVIrc-4.2.0-Equilibrium.exe
 ;OutFile KVIrc-${VERSION}-${RELEASE_NAME}.exe
 
+Unicode on
 SetCompressor /SOLID lzma
 XPStyle on
 InstallDir $PROGRAMFILES\KVIrc
