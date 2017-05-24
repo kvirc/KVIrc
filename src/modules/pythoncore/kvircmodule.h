f5d172c869295ef9fff2e3de524690e523dc5d42
Alexey Sokolov <alexey+github@asokolov.org>
2017-05-24 10:04:20 +0100

    Build also in debug mode for windows (#2192)
    * Build also in debug mode for windows
    
    See #2191
    
    * Don't mix /Od and -O2 on windows build. -O2 comes from perl
    
    * Reindent perlcore CMakeLists
    
    * Fix debug python build on windows
    
    * Rearrange environment vars in appveyor
    
    * Try to publish pdb files too
    
    * install qts and kvirc's debug pdbs in debug config
    
    * use TARGET_PDB_FILE instead

--- src/modules/pythoncore/kvircmodule.h
+++ src/modules/pythoncore/kvircmodule.h
@@ -37,7 +37,7 @@
 
 #ifdef COMPILE_PYTHON_SUPPORT
 
-#include <Python.h>
+#include "pythonheaderwrapper.h"
 
 // python included like to pollute the global namespace, let's #undef some beef
 #undef isspace
