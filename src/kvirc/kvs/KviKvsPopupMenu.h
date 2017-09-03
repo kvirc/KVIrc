20efc5d86287af0708bc26a7eb6c5f6d7f32178c
IceN9ne <IceN9ne.code@gmail.com>
2017-09-02 20:09:04 -0400

    KviKvsPopupMenu: Code cleanup
        * Fixes #2295

--- src/kvirc/kvs/KviKvsPopupMenu.h
+++ src/kvirc/kvs/KviKvsPopupMenu.h
@@ -53,23 +53,22 @@ public:
 	};
 
 protected:
-	KviKvsExtendedRunTimeData * m_pExtendedRunTimeData;
-	KviKvsVariantList * m_pParameters;
-	KviWindow * m_pWindow;
-	LockStatus m_eLocked;
-	bool m_bTestMode;
+	KviKvsExtendedRunTimeData * m_pExtendedRunTimeData = nullptr;
+	KviKvsVariantList * m_pParameters = nullptr;
+	KviWindow * m_pWindow = nullptr;
+	LockStatus m_eLocked = Unlocked;
+	bool m_bTestMode = false;
 
 public:
-	KviKvsExtendedRunTimeData * extendedRunTimeData() { return m_pExtendedRunTimeData; };
-	//KviKvsHash * extScopeVariables(){ return m_pExtScopeVariables; };
-	KviKvsVariantList * parameters() { return m_pParameters; };
-	bool isSoftLocked() { return m_eLocked != Unlocked; };
-	bool isHardLocked() { return m_eLocked == HardLocked; };
-	void setLocked(LockStatus eLocked) { m_eLocked = eLocked; };
-	KviWindow * window() { return m_pWindow; };
-	void setWindow(KviWindow * pWindow) { m_pWindow = pWindow; };
-	bool testMode() { return m_bTestMode; };
-	void setTestMode(bool bTestMode) { m_bTestMode = bTestMode; };
+	KviKvsExtendedRunTimeData * extendedRunTimeData() const { return m_pExtendedRunTimeData; }
+	KviKvsVariantList * parameters() const { return m_pParameters; }
+	bool isSoftLocked() const { return m_eLocked != Unlocked; }
+	bool isHardLocked() const { return m_eLocked == HardLocked; }
+	void setLocked(LockStatus eLocked) { m_eLocked = eLocked; }
+	KviWindow * window() const { return m_pWindow; }
+	void setWindow(KviWindow * pWindow) { m_pWindow = pWindow; }
+	bool testMode() const { return m_bTestMode; }
+	void setTestMode(bool bTestMode) { m_bTestMode = bTestMode; }
 };
 
 class KVIRC_API KviKvsPopupMenuItem
@@ -96,26 +95,26 @@ public:
 protected:
 	QString m_szItemName;
 	Type m_eType;
-	KviKvsScript * m_pKvsCondition;
+	KviKvsScript * m_pKvsCondition = nullptr;
 
 public:
 	// this doesn't trigger errors, only warnings
 	bool evaluateCondition(KviKvsPopupMenuTopLevelData * pData);
 
-	KviKvsPopupMenuItem::Type type() { return m_eType; };
+	KviKvsPopupMenuItem::Type type() const { return m_eType; }
 
-	const QString & name() { return m_szItemName; };
+	const QString & name() const { return m_szItemName; }
 
-	bool isItem() { return m_eType == Item; };
-	bool isSeparator() { return m_eType == Separator; };
-	bool isLabel() { return m_eType == Label; };
-	bool isExtMenu() { return m_eType == ExtMenu; };
-	bool isMenu() { return m_eType == Menu; };
+	bool isItem() const { return m_eType == Item; }
+	bool isSeparator() const { return m_eType == Separator; }
+	bool isLabel() const { return m_eType == Label; }
+	bool isExtMenu() const { return m_eType == ExtMenu; }
+	bool isMenu() const { return m_eType == Menu; }
 
-	KviKvsScript * kvsCondition() { return m_pKvsCondition; };
-	virtual KviKvsScript * kvsIcon();
-	virtual KviKvsScript * kvsText();
-	virtual KviKvsScript * kvsCode();
+	KviKvsScript * kvsCondition() const { return m_pKvsCondition; }
+	virtual KviKvsScript * kvsIcon() const;
+	virtual KviKvsScript * kvsText() const;
+	virtual KviKvsScript * kvsCode() const;
 
 	virtual void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx) = 0;
 	virtual void clear();
@@ -131,11 +130,11 @@ protected:
 	KviKvsPopupMenuItemSeparator(const QString & szItemName, const KviKvsScript * pCondition);
 
 public:
-	virtual ~KviKvsPopupMenuItemSeparator();
+	~KviKvsPopupMenuItemSeparator();
 
 public:
-	virtual void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx);
-	virtual KviKvsPopupMenuItem * clone() const;
+	void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx) override;
+	KviKvsPopupMenuItem * clone() const override;
 };
 
 class KVIRC_API KviKvsPopupMenuItemWithTextAndIcon : public KviKvsPopupMenuItem
@@ -145,15 +144,15 @@ protected:
 	KviKvsPopupMenuItemWithTextAndIcon(KviKvsPopupMenuItem::Type t, const QString & szItemName, const KviKvsScript * pText, const KviKvsScript * pIcon, const KviKvsScript * pCondition);
 
 public:
-	virtual ~KviKvsPopupMenuItemWithTextAndIcon();
+	~KviKvsPopupMenuItemWithTextAndIcon();
 
 protected:
-	KviKvsScript * m_pKvsText;
-	KviKvsScript * m_pKvsIcon;
+	KviKvsScript * m_pKvsText = nullptr;
+	KviKvsScript * m_pKvsIcon = nullptr;
 
 public:
-	virtual KviKvsScript * kvsIcon();
-	virtual KviKvsScript * kvsText();
+	KviKvsScript * kvsIcon() const override;
+	KviKvsScript * kvsText() const override;
 	// this just returns a string, eventually empty
 	QString evaluateText(KviKvsPopupMenuTopLevelData * pData);
 	// this just returns the icon, eventually
@@ -173,7 +172,7 @@ protected:
 	~KviKvsPopupMenuItemLabelHelper();
 
 protected:
-	KviKvsPopupMenuItemLabel * m_pItem;
+	KviKvsPopupMenuItemLabel * m_pItem = nullptr;
 protected slots:
 	void labelDestroyed();
 };
@@ -188,16 +187,16 @@ protected:
 	KviKvsPopupMenuItemLabel(const QString & szItemName, const KviKvsScript * pText, const KviKvsScript * pIcon, const KviKvsScript * pCondition);
 
 public:
-	virtual ~KviKvsPopupMenuItemLabel();
+	~KviKvsPopupMenuItemLabel();
 
 protected:
-	QLabel * m_pLabel;
-	KviKvsPopupMenuItemLabelHelper * m_pSignalRelay;
+	QLabel * m_pLabel = nullptr;
+	KviKvsPopupMenuItemLabelHelper * m_pSignalRelay = nullptr;
 
 public:
-	virtual void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx);
-	virtual KviKvsPopupMenuItem * clone() const;
-	virtual void clear();
+	void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx) override;
+	KviKvsPopupMenuItem * clone() const override;
+	void clear() override;
 
 protected:
 	void labelDestroyed();
@@ -212,17 +211,17 @@ protected:
 	KviKvsPopupMenuItemItem(const QString & szItemName, const KviKvsScript * pCode, const KviKvsScript * pText, const KviKvsScript * pIcon, const KviKvsScript * pCondition);
 
 public:
-	virtual ~KviKvsPopupMenuItemItem();
+	~KviKvsPopupMenuItemItem();
 
 protected:
-	KviKvsScript * m_pKvsCode;
+	KviKvsScript * m_pKvsCode = nullptr;
 
 protected:
-	virtual KviKvsScript * kvsCode();
+	KviKvsScript * kvsCode() const override;
 
 public:
-	virtual void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx);
-	virtual KviKvsPopupMenuItem * clone() const;
+	void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx) override;
+	KviKvsPopupMenuItem * clone() const override;
 };
 
 class KVIRC_API KviKvsPopupMenuItemMenu : public KviKvsPopupMenuItemWithTextAndIcon
@@ -234,16 +233,16 @@ protected:
 	KviKvsPopupMenuItemMenu(const QString & szItemName, KviKvsPopupMenu * pMenu, const KviKvsScript * pText, const KviKvsScript * pIcon, const KviKvsScript * pCondition);
 
 public:
-	virtual ~KviKvsPopupMenuItemMenu();
+	~KviKvsPopupMenuItemMenu();
 
 protected:
-	KviKvsPopupMenu * m_pMenu;
+	KviKvsPopupMenu * m_pMenu = nullptr;
 
 public:
-	KviKvsPopupMenu * menu() { return m_pMenu; };
-	virtual void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx);
-	virtual void clear();
-	virtual KviKvsPopupMenuItem * clone() const;
+	KviKvsPopupMenu * menu() const { return m_pMenu; }
+	void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx) override;
+	void clear() override;
+	KviKvsPopupMenuItem * clone() const override;
 };
 
 class KVIRC_API KviKvsPopupMenuItemExtMenu : public KviKvsPopupMenuItemWithTextAndIcon
@@ -255,48 +254,49 @@ protected:
 	KviKvsPopupMenuItemExtMenu(const QString & szItemName, const QString & szMenuName, const KviKvsScript * pText, const KviKvsScript * pIcon, const KviKvsScript * pCondition);
 
 public:
-	virtual ~KviKvsPopupMenuItemExtMenu();
+	~KviKvsPopupMenuItemExtMenu();
 
 protected:
 	QString m_szMenuName;
-	KviKvsPopupMenu * m_pMenu; // owned!
+	KviKvsPopupMenu * m_pMenu = nullptr; // owned!
 public:
-	const QString & extName() { return m_szMenuName; };
-	virtual void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx);
-	virtual void clear();
-	virtual KviKvsPopupMenuItem * clone() const;
+	const QString & extName() const { return m_szMenuName; }
+	void fill(KviKvsPopupMenu * pMenu, KviKvsPopupMenuTopLevelData * pData, int iIdx) override;
+	void clear() override;
+	KviKvsPopupMenuItem * clone() const override;
 };
 
 class KVIRC_API KviKvsPopupMenu : public QMenu
 {
+	Q_OBJECT
 	friend class KviKvsPopupMenuItemMenu;
 	friend class KviKvsPopupManager;
 	friend class KviSinglePopupEditor;
-	Q_OBJECT
+
 public:
 	KviKvsPopupMenu(const QString & szName);
 	~KviKvsPopupMenu();
 
 protected:
-	KviPointerList<KviKvsPopupMenuItem> * m_pItemList;
+	KviPointerList<KviKvsPopupMenuItem> * m_pItemList = nullptr;
 
 private:
 	QString m_szName;
-	KviKvsPopupMenu * m_pParentPopup;
-	KviPointerList<KviKvsScript> * m_pPrologues;
-	KviPointerList<KviKvsScript> * m_pEpilogues;
-	KviKvsPopupMenuTopLevelData * m_pTopLevelData;
+	KviKvsPopupMenu * m_pParentPopup = nullptr;
+	KviPointerList<KviKvsScript> * m_pPrologues = nullptr;
+	KviPointerList<KviKvsScript> * m_pEpilogues = nullptr;
+	KviKvsPopupMenuTopLevelData * m_pTopLevelData = nullptr;
 	// this is a temporary used to hack-in the activation from KviMenuBar
-	KviKvsPopupMenuTopLevelData * m_pTempTopLevelData;
-	bool m_bSetupDone;
+	KviKvsPopupMenuTopLevelData * m_pTempTopLevelData = nullptr;
+	bool m_bSetupDone = false;
 
 public:
-	const QString & popupName() { return m_szName; };
-	void setPopupName(const QString & szName) { m_szName = szName; };
+	const QString & popupName() const { return m_szName; }
+	void setPopupName(const QString & szName) { m_szName = szName; }
 	void copyFrom(const KviKvsPopupMenu * src);
 	KviKvsPopupMenuTopLevelData * topLevelData();
 	KviKvsPopupMenu * topLevelPopup();
-	KviPointerList<KviKvsPopupMenuItem> * itemList() { return m_pItemList; };
+	KviPointerList<KviKvsPopupMenuItem> * itemList() const { return m_pItemList; }
 	bool isSoftLocked();
 	bool isHardLocked();
 	void lock(KviKvsPopupMenuTopLevelData::LockStatus eLock);
@@ -306,17 +306,17 @@ public:
 	void addItem(const QString & szItemName, const QString & szCode, const QString & szText, const QString & szIcon, const QString & szCondition);
 	void addExtPopup(const QString & szItemName, const QString & szPopupName, const QString & szText, const QString & szIcon, const QString & szCondition);
 	void doPopup(const QPoint & pnt, KviWindow * wnd, KviKvsVariantList * pParams, bool bTestMode = false);
-	bool isEmpty() { return m_pItemList->isEmpty() && m_pPrologues->isEmpty() && m_pEpilogues->isEmpty(); };
+	bool isEmpty() const { return m_pItemList->isEmpty() && m_pPrologues->isEmpty() && m_pEpilogues->isEmpty(); }
 	void doClear();
 	void addPrologue(const QString & szItemName, const QString & szCode);
 	void addEpilogue(const QString & szItemName, const QString & szCode);
 	bool removeItemByName(const QString & szItemName, bool bRecursive);
 	KviKvsPopupMenu * findChildPopupByName(const QString & szItemName);
-	KviPointerList<KviKvsScript> * epilogues() { return m_pEpilogues; };
-	KviPointerList<KviKvsScript> * prologues() { return m_pPrologues; };
-	KviKvsPopupMenu * parentPopup() { return m_pParentPopup; };
+	KviPointerList<KviKvsScript> * epilogues() const { return m_pEpilogues; }
+	KviPointerList<KviKvsScript> * prologues() const { return m_pPrologues; }
+	KviKvsPopupMenu * parentPopup() const { return m_pParentPopup; }
 	void generateDefPopup(QString & buffer);
-	void setParentPopup(KviKvsPopupMenu * par) { m_pParentPopup = par; };
+	void setParentPopup(KviKvsPopupMenu * par) { m_pParentPopup = par; }
 	void generateDefPopupCore(QString & buffer);
 
 protected:
