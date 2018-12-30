20e1ae1eb8131f854dfd9217e545ee0b080eeba8
Alexander Pozdnyakov <almipo@mail.ru>
2018-12-30 16:55:02 +0300

    build_debian: debuild stdout and stderr to /dev/null

--- dist/build_debian.sh
+++ dist/build_debian.sh
@@ -117,7 +117,8 @@ then
     if [ ! -z "${TRAVIS_BRANCH}" ]
     then
         sed -i '/^ cmake (>= 3.1),/d' debian/control
-        debuild --no-lintian --preserve-env --prepend-path="${PATH}" -us -uc 
+        debuild --no-lintian --preserve-env --prepend-path="${PATH}" -us -uc > /dev/null 2>&1
+        tail -50 ../kvirc_*.build
     else
         debuild --no-lintian -us -uc 
     fi
