6df26cc536b1dc648b31d998f81a92c0d3746206
IceN9ne <IceN9ne.code@gmail.com>
2018-12-31 09:40:33 -0500

    KviMenuBar: Update code
    Will need to wait until a newer Qt is used to fully convert to Qt5 signal and slot syntax.

--- src/kvirc/ui/KviMenuBar.cpp
+++ src/kvirc/ui/KviMenuBar.cpp
@@ -23,78 +23,74 @@
 //=============================================================================
 
 #include "KviMenuBar.h"
+#include "KviActionManager.h"
 #include "KviApplication.h"
-#include "KviLocale.h"
-#include "KviMainWindow.h"
-#include "KviWindowStack.h"
+#include "KviConsoleWindow.h"
+#include "KviCoreActionNames.h"
 #include "KviIconManager.h"
 #include "KviInternalCommand.h"
 #include "KviIrcUrl.h"
-#include "KviConsoleWindow.h"
 #include "KviKvsPopupMenu.h"
+#include "KviKvsScript.h"
+#include "KviLocale.h"
+#include "KviMainWindow.h"
 #include "KviMemory.h"
 #include "KviModuleExtension.h"
 #include "KviOptions.h"
-#include "KviActionManager.h"
-#include "KviCoreActionNames.h"
-#include "KviKvsScript.h"
 #include "KviShortcut.h"
-#include "KviOptions.h"
+#include "KviWindowStack.h"
 
 #include <QKeySequence>
 #include <QMenu>
 
 KviMenuBar::KviMenuBar(KviMainWindow * par, const char * name)
-    : KviTalMenuBar(par, name)
+    : KviTalMenuBar(par, name), m_pFrm{par}
 {
 	setAutoFillBackground(false);
-	m_pFrm = par;
 
-	m_pRecentServersPopup = new QMenu("recentservers", this);
-	connect(m_pRecentServersPopup, SIGNAL(aboutToShow()), this, SLOT(updateRecentServersPopup()));
-	connect(m_pRecentServersPopup, SIGNAL(triggered(QAction *)), this, SLOT(newConnectionToServer(QAction *)));
+	m_pRecentServersPopup = new QMenu(QStringLiteral("recentservers"), this);
+	connect(m_pRecentServersPopup, &QMenu::aboutToShow, this, &KviMenuBar::updateRecentServersPopup);
+	connect(m_pRecentServersPopup, &QMenu::triggered, this, &KviMenuBar::newConnectionToServer);
 
-	m_pModulesToolsPopup = new QMenu("modulestools", this);
-	connect(m_pModulesToolsPopup, SIGNAL(aboutToShow()), this, SLOT(updateModulesToolsPopup()));
-	connect(m_pModulesToolsPopup, SIGNAL(triggered(QAction *)), this, SLOT(modulesToolsTriggered(QAction *)));
+	m_pModulesToolsPopup = new QMenu(QStringLiteral("modulestools"), this);
+	connect(m_pModulesToolsPopup, &QMenu::aboutToShow, this, &KviMenuBar::updateModulesToolsPopup);
+	connect(m_pModulesToolsPopup, &QMenu::triggered, this, &KviMenuBar::modulesToolsTriggered);
 
-	m_pActionsToolsPopup = new QMenu("actionstools", this);
-	connect(m_pActionsToolsPopup, SIGNAL(aboutToShow()), this, SLOT(updateActionsToolsPopup()));
+	m_pActionsToolsPopup = new QMenu(QStringLiteral("actionstools"), this);
+	connect(m_pActionsToolsPopup, &QMenu::aboutToShow, this, &KviMenuBar::updateActionsToolsPopup);
 
-	QMenu * pop = new QMenu("KVIrc", this);
+	QMenu * pop = new QMenu(QStringLiteral("KVIrc"), this);
 	setupMainPopup(pop);
-	connect(pop, SIGNAL(aboutToShow()), this, SLOT(updateMainPopup()));
+	connect(pop, &QMenu::aboutToShow, this, &KviMenuBar::updateMainPopup);
 #ifndef COMPILE_ON_MAC
-	addDefaultItem("&KVIrc", pop);
+	addDefaultItem(QStringLiteral("&KVIrc"), pop);
 #else
 	// Qt/Mac creates already a "KVirc" menu item on its own, and we don't like double entries ;-)
-	addDefaultItem("&IRC", pop);
+	addDefaultItem(QStringLiteral("&IRC"), pop);
 #endif //COMPILE_ON_MAC
 
-	m_pScriptItemList = nullptr;
-
-	pop = new QMenu("scripting", this);
+	pop = new QMenu(QStringLiteral("scripting"), this);
 	setupScriptingPopup(pop);
 	addDefaultItem(__tr2qs("Scri&pting"), pop);
 
-	pop = new QMenu("tools", this);
+	pop = new QMenu(QStringLiteral("tools"), this);
 	setupToolsPopup(pop);
 	addDefaultItem(__tr2qs("&Tools"), pop);
-	connect(pop, SIGNAL(aboutToShow()), this, SLOT(updateToolsPopup()));
+	connect(pop, &QMenu::aboutToShow, this, &KviMenuBar::updateToolsPopup);
 
-	m_pToolbarsPopup = new QMenu("toolbars", this);
-	connect(m_pToolbarsPopup, SIGNAL(aboutToShow()), this, SLOT(updateToolbarsPopup()));
+	m_pToolbarsPopup = new QMenu(QStringLiteral("toolbars"), this);
+	connect(m_pToolbarsPopup, &QMenu::aboutToShow, this, &KviMenuBar::updateToolbarsPopup);
 
-	pop = new QMenu("settings", this);
+	pop = new QMenu(QStringLiteral("settings"), this);
 	setupSettingsPopup(pop);
-	connect(pop, SIGNAL(aboutToShow()), this, SLOT(updateSettingsPopup()));
+	connect(pop, &QMenu::aboutToShow, this, &KviMenuBar::updateSettingsPopup);
 	addDefaultItem(__tr2qs("&Settings"), pop);
 
 	addDefaultItem(__tr2qs("&Window"), par->windowStack()->windowPopup());
 
-	pop = new QMenu("help", this);
+	pop = new QMenu(QStringLiteral("help"), this);
 	setupHelpPopup(pop);
-	connect(pop, SIGNAL(triggered(QAction *)), this, SLOT(actionTriggered(QAction *)));
+	connect(pop, &QMenu::triggered, this, &KviMenuBar::actionTriggered);
 	addDefaultItem(__tr2qs("&Help"), pop);
 }
 
@@ -112,7 +108,7 @@ void KviMenuBar::addDefaultItem(const QString & text, QMenu * pop)
 
 void KviMenuBar::setupHelpPopup(QMenu * pop)
 {
-	QMenu * help = pop ? pop : (QMenu *)sender();
+	QMenu * help = pop ? pop : qobject_cast<QMenu *>(sender());
 	help->clear();
 
 	ACTION_POPUP_ITEM(KVI_COREACTION_HELPINDEX, help)
@@ -128,7 +124,7 @@ void KviMenuBar::setupHelpPopup(QMenu * pop)
 	help->addSeparator();
 	pAction = help->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::HomePage)), __tr2qs("KVIrc Home&page"));
 	pAction->setData(KVI_INTERNALCOMMAND_KVIRC_HOMEPAGE);
-	if(QString::compare(KviLocale::instance()->localeName(), QString("ru"), Qt::CaseInsensitive) == 0)
+	if(QString::compare(KviLocale::instance()->localeName(), QLatin1String("ru"), Qt::CaseInsensitive) == 0)
 	{
 		pAction = help->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::HomePage)), __tr2qs("KVIrc Russian Home&page"));
 		pAction->setData(KVI_INTERNALCOMMAND_KVIRC_HOMEPAGE_RU);
@@ -143,15 +139,15 @@ void KviMenuBar::setupHelpPopup(QMenu * pop)
 
 void KviMenuBar::actionTriggered(QAction * pAction)
 {
-	bool bOk = false;
+	bool bOk;
 	int id = pAction->data().toInt(&bOk);
 	if(bOk)
 		m_pFrm->executeInternalCommand(id);
 }
 
-void KviMenuBar::actionTriggered(bool)
+void KviMenuBar::actionTriggeredBool(bool)
 {
-	QAction * pAction = (QAction *)sender();
+	QAction * pAction = qobject_cast<QAction *>(sender());
 	if(!pAction)
 		return;
 
@@ -169,7 +165,7 @@ void KviMenuBar::updateSettingsPopup()
 
 void KviMenuBar::setupSettingsPopup(QMenu * pop)
 {
-	QMenu * opt = pop ? pop : (QMenu *)sender();
+	QMenu * opt = pop ? pop : qobject_cast<QMenu *>(sender());
 	opt->clear();
 
 	QAction * pAction = opt->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Toolbar)), __tr2qs("Toolbars"));
@@ -206,7 +202,7 @@ void KviMenuBar::setupSettingsPopup(QMenu * pop)
 
 void KviMenuBar::setupScriptingPopup(QMenu * pop)
 {
-	QMenu * script = pop ? pop : (QMenu *)sender();
+	QMenu * script = pop ? pop : qobject_cast<QMenu *>(sender());
 	script->clear();
 
 	ACTION_POPUP_ITEM(KVI_COREACTION_ACTIONEDITOR, script)
@@ -232,7 +228,7 @@ void KviMenuBar::updateMainPopup()
 
 void KviMenuBar::setupMainPopup(QMenu * pop)
 {
-	QMenu * main = pop ? pop : (QMenu *)sender();
+	QMenu * main = pop ? pop : qobject_cast<QMenu *>(sender());
 	main->clear();
 
 	ACTION_POPUP_ITEM(KVI_COREACTION_NEWIRCCONTEXT, main)
@@ -240,7 +236,7 @@ void KviMenuBar::setupMainPopup(QMenu * pop)
 	QAction * pAction = main->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::World)), __tr2qs("New &Connection to"));
 	pAction->setMenu(m_pRecentServersPopup);
 
-	m_pDisconnectAction = main->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Quit)), __tr2qs("Disconnect"), this, SLOT(actionTriggered(bool)));
+	m_pDisconnectAction = main->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Quit)), __tr2qs("Disconnect"), this, SLOT(actionTriggeredBool(bool)));
 	m_pDisconnectAction->setData(KVI_INTERNALCOMMAND_QUIT);
 
 // FIXME: Add a "Dock to tray" icon if the tray is not visible (or show tray icon or whatever)
@@ -255,7 +251,7 @@ void KviMenuBar::setupMainPopup(QMenu * pop)
 
 void KviMenuBar::updateRecentServersPopup()
 {
-	QMenu * m = (QMenu *)sender();
+	QMenu * m = qobject_cast<QMenu *>(sender());
 	g_pApp->fillRecentServersPopup(m);
 	m->addSeparator();
 	m->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Server)), __tr2qs("&Other..."));
@@ -272,7 +268,7 @@ void KviMenuBar::newConnectionToServer(QAction * pAction)
 		}
 		else
 		{
-			text.replace(QString("&"), QString(""));
+			text.replace(QLatin1String("&"), QLatin1String(""));
 			KviCString szCommand;
 			if(KviIrcUrl::parse(text.toUtf8().data(), szCommand, KVI_IRCURL_CONTEXT_FIRSTFREE))
 			{
@@ -296,20 +292,17 @@ void KviMenuBar::updateModulesToolsPopup()
 {
 	m_pModulesToolsPopup->clear();
 
-	QAction * pAction = nullptr;
 	KviModuleExtensionDescriptorList * l = g_pModuleExtensionManager->getExtensionList("tool");
 	if(l)
 	{
 		for(KviModuleExtensionDescriptor * d = l->first(); d; d = l->next())
 		{
+			QAction * pAction = nullptr;
 			if(d->icon())
-			{
 				pAction = m_pModulesToolsPopup->addAction(*(d->icon()), d->visibleName());
-			}
 			else
-			{
 				pAction = m_pModulesToolsPopup->addAction(d->visibleName());
-			}
+
 			pAction->setData(d->id());
 		}
 	}
@@ -331,7 +324,7 @@ void KviMenuBar::updateActionsToolsPopup()
 
 void KviMenuBar::setupToolsPopup(QMenu * pop)
 {
-	QMenu * m = pop ? pop : (QMenu *)sender();
+	QMenu * m = pop ? pop : qobject_cast<QMenu *>(sender());
 	if(!m)
 		return;
 
@@ -359,8 +352,7 @@ void KviMenuBar::setupToolsPopup(QMenu * pop)
 	// moved the old tools here
 	m->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::IconManager)), __tr2qs("Show &Icon Table"), g_pIconManager, SLOT(showIconWidget()));
 #ifdef COMPILE_KDE4_SUPPORT
-	QAction * pAction = nullptr;
-	pAction = m->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Terminal)), __tr2qs("Open &Terminal"), this, SLOT(actionTriggered(bool)));
+	QAction * pAction = m->addAction(*(g_pIconManager->getSmallIcon(KviIconManager::Terminal)), __tr2qs("Open &Terminal"), this, SLOT(actionTriggeredBool(bool)));
 	pAction->setData(KVI_INTERNALCOMMAND_TERM_OPEN);
 #endif
 
@@ -375,7 +367,7 @@ void KviMenuBar::setupToolsPopup(QMenu * pop)
 
 void KviMenuBar::modulesToolsTriggered(QAction * pAction)
 {
-	bool bOk = false;
+	bool bOk;
 	int idext = pAction->data().toInt(&bOk);
 	if(bOk)
 		g_pModuleExtensionManager->allocateExtension("tool", idext, m_pFrm->firstConsole());
@@ -392,12 +384,7 @@ void KviMenuBar::updateToolbarsPopup()
 
 int KviMenuBar::getDefaultItemRealIndex(int iDefaultIndex)
 {
-	if(iDefaultIndex < 0)
-		iDefaultIndex = 0;
-	if(iDefaultIndex > actions().count())
-		iDefaultIndex = actions().count();
-
-	return iDefaultIndex;
+	return std::clamp(iDefaultIndex, 0, actions().count());
 }
 
 KviScriptMenuBarItem * KviMenuBar::findMenu(const QString & text)
@@ -405,10 +392,9 @@ KviScriptMenuBarItem * KviMenuBar::findMenu(const QString & text)
 	if(!m_pScriptItemList)
 		return nullptr;
 	for(KviScriptMenuBarItem * i = m_pScriptItemList->first(); i; i = m_pScriptItemList->next())
-	{
 		if(KviQString::equalCI(text, i->szText))
 			return i;
-	}
+
 	return nullptr;
 }
 
@@ -417,19 +403,17 @@ KviScriptMenuBarItem * KviMenuBar::findMenu(KviKvsPopupMenu * p)
 	if(!m_pScriptItemList)
 		return nullptr;
 	for(KviScriptMenuBarItem * i = m_pScriptItemList->first(); i; i = m_pScriptItemList->next())
-	{
 		if(i->pPopup == p)
 			return i;
-	}
+
 	return nullptr;
 }
 
 bool KviMenuBar::removeMenu(const QString & text)
 {
-	KviScriptMenuBarItem * i = findMenu(text);
-	if(i)
+	if(KviScriptMenuBarItem * i = findMenu(text); i)
 	{
-		disconnect(i->pPopup, SIGNAL(destroyed()), this, SLOT(menuDestroyed()));
+		disconnect(i->pPopup, &KviKvsPopupMenu::destroyed, this, &KviMenuBar::menuDestroyed);
 		removeAction(i->pPopup->menuAction());
 		m_pScriptItemList->removeRef(i);
 		return true;
@@ -439,8 +423,7 @@ bool KviMenuBar::removeMenu(const QString & text)
 
 void KviMenuBar::menuDestroyed()
 {
-	KviScriptMenuBarItem * i = findMenu(((KviKvsPopupMenu *)sender()));
-	if(i)
+	if(KviScriptMenuBarItem * i = findMenu(qobject_cast<KviKvsPopupMenu *>(sender())); i)
 	{
 		// No need to remove the associated action: qt already did it (ticket #931)
 		m_pScriptItemList->removeRef(i);
@@ -461,14 +444,10 @@ void KviMenuBar::addMenu(const QString & text, KviKvsPopupMenu * p, int index)
 	it->pPopup = p;
 	it->pPopup->menuAction()->setText(text);
 	if(index == -1 || index >= actions().count())
-	{
 		addAction(it->pPopup->menuAction());
-	}
 	else
-	{
 		insertAction(actions().value(index), it->pPopup->menuAction());
-	}
 
-	connect(p, SIGNAL(destroyed()), this, SLOT(menuDestroyed()));
+	connect(p, &KviKvsPopupMenu::destroyed, this, &KviMenuBar::menuDestroyed);
 	m_pScriptItemList->append(it);
 }
