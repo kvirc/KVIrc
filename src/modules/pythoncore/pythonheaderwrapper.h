79dbba4260113b453fcea155881ecb4c6f82963b
Alexey Sokolov <sokolov@google.com>
2019-12-30 07:32:51 +0000

    Stop unsetting _DEBUG when including Python.h
    Because of https://stackoverflow.com/questions/59126760/building-a-python-c-extension-on-windows-with-a-debug-python-installation

--- src/modules/pythoncore/pythonheaderwrapper.h
+++ src/modules/pythoncore/pythonheaderwrapper.h
@@ -7,14 +7,6 @@
 // use QT_NO_KEYWORDS.
 #undef slots
 
-// See http://stackoverflow.com/questions/16200997/why-doesnt-include-python-h-work and http://stackoverflow.com/questions/19716859/puzzling-dependency-of-boost-python-1-54-debug-build-to-python27-lib-on-window
-
-#if defined(_DEBUG) && defined(_MSC_VER)
-# undef _DEBUG
-# include <Python.h>
-# define _DEBUG 1
-#else
-# include <Python.h>
-#endif
+#include <Python.h>
 
 #endif
