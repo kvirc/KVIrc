9b8fa92ef5cceccfbb463ccd18cab89d60d5d836
ctrlaltca <ctrlaltca@gmail.com>
2024-02-11 16:36:22 +0100

    Replace deprecated define. Fix #2601 (#2603)

--- src/kvilib/config/kvi_settings.h
+++ src/kvilib/config/kvi_settings.h
@@ -128,8 +128,8 @@
 #define KVIRC_API_TYPEDEF
 
 // note: cmake defines "APPLE" but we're not using it since it includes legacy mac os version (pre-X)
-// we use qt's Q_OS_MACX instead
-#ifdef Q_OS_MACX
+// we use qt's Q_OS_MACOS instead
+#ifdef Q_OS_MACOS
 /**
 		* \def COMPILE_ON_MAC This flag will enable specific code for mac compilation
 		*/
