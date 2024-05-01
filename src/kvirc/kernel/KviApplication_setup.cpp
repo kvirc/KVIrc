2dc7dfe1e10f30d933a1bd6b5281d25393c1ec06
ctrlaltca <ctrlaltca@gmail.com>
2024-05-01 19:07:56 +0200

    Add cmake option WANT_PERL_DOCS to disable doc generation.  (#2635)
    * Add cmake option WANT_PERL_DOCS to disable doc generation. Ensure Qt version is reported correctly by cmake. Fix #2634
    
    * KviApplication::checkGlobalKvircDirectory: don't test for help subdir, it may not exist
    
    * Cmake: use the imported target for Threads instead of CMAKE_THREAD_LIBS_INIT
    
    * Cmake: always import the Threads target

--- src/kvirc/kernel/KviApplication_setup.cpp
+++ src/kvirc/kernel/KviApplication_setup.cpp
@@ -63,10 +63,10 @@ static QLibrary * g_pSetupLibrary = nullptr;
 
 bool KviApplication::checkGlobalKvircDirectory(const QString szDir)
 {
-	//First check if the help subdir exists
-	QString szHelpDir = szDir;
-	szHelpDir += KVI_PATH_SEPARATOR "help";
-	if(!KviFileUtils::directoryExists(szHelpDir))
+	//First check if the config subdir exists
+	QString szConfigDir = szDir;
+	szConfigDir += KVI_PATH_SEPARATOR "config";
+	if(!KviFileUtils::directoryExists(szConfigDir))
 		return false;
 	//Then check if the pics subdir exists
 	QString szPicsDir = szDir;
