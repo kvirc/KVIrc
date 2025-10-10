b4e0ba463ec603d93022246536fb32ec2d651c82
Bartkk <bigdabartlomiej@gmail.com>
2025-10-11 00:17:16 +0200

    Fix DBUS notifier arguments (#2715)
    * Fix DBUS notifier arguments
    
    * Replace unsigned int cast with a suffix

--- src/kvirc/kernel/KviApplication.cpp
+++ src/kvirc/kernel/KviApplication.cpp
@@ -852,7 +852,7 @@ void KviApplication::notifierMessage(KviWindow * pWnd, int iIconId, const QStrin
 			// org.freedesktop.Notifications.Notify
 			QVariantList args;
 			args << QString("KVIrc");                          // application name
-			args << QVariant(0);                               // notification id
+			args << QVariant(0u);                              // notification id
 			args << szIcon;                                    // application icon
 			args << szTitle;                                   // summary text
 			args << szText;                                    // detailed text
